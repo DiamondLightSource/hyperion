84b2dea3615d41266e266ff659f38a47dbb85ab1
Dominic Oram <dominic.oram@diamond.ac.uk>
2022-01-20 12:02:57 +0000

    8: Minor tidying up after review

--- src/artemis/devices/fast_grid_scan.py
+++ src/artemis/devices/fast_grid_scan.py
@@ -9,7 +9,8 @@ from ophyd import (
     EpicsSignalWithRBV,
     Signal,
 )
-from ophyd.status import DeviceStatus, StatusBase, SubscriptionStatus
+from ophyd.status import DeviceStatus, StatusBase
+from ophyd.utils.epics_pvs import set_and_wait
 
 from dataclasses import dataclass
 from typing import Any
@@ -20,6 +21,7 @@ from src.artemis.devices.motors import (
 )
 
 from bluesky.plan_stubs import mv
+from src.artemis.devices.status import await_value
 
 
 @dataclass
@@ -180,12 +182,9 @@ class FastGridScan(Device):
                 while self.is_invalid() or not self.position_counter.get() == 0:
                     time.sleep(0.1)
                 self.log.debug("Running scan")
-                running = SubscriptionStatus(
-                    self.status, lambda old_value, value, **kwargs: value == 1
-                )
                 self.run_cmd.put(1)
                 self.log.debug("Waiting for scan to start")
-                running.wait()
+                await_value(self.status, 1).wait()
                 st.set_finished()
             except Exception as e:
                 st.set_exception(e)
@@ -194,8 +193,7 @@ class FastGridScan(Device):
         return st
 
     def stage(self) -> List[object]:
-        status = self.position_counter.set(0)
-        status.wait()
+        set_and_wait(self.position_counter, 0)
         return super().stage()
 
     def complete(self) -> DeviceStatus:
--- src/artemis/devices/system_tests/test_gridscan_system.py
+++ src/artemis/devices/system_tests/test_gridscan_system.py
@@ -15,14 +15,9 @@ def fast_grid_scan():
 
 
 @pytest.mark.s03
-def test_can_set_program_data_and_expected_images_correct(fast_grid_scan: FastGridScan):
-    RE = RunEngine()
-    RE(set_fast_grid_scan_params(fast_grid_scan, GridScanParams(2, 2)))
-    assert fast_grid_scan.expected_images.get() == 2 * 2
-
-
-@pytest.mark.s03
-def test_staging_fast_grid_scan_clears_position_counter(fast_grid_scan: FastGridScan):
+def test_when_program_data_set_and_staged_then_expected_images_correct(
+    fast_grid_scan: FastGridScan,
+):
     RE = RunEngine()
     RE(set_fast_grid_scan_params(fast_grid_scan, GridScanParams(2, 2)))
     assert fast_grid_scan.expected_images.get() == 2 * 2
@@ -52,6 +47,10 @@ def test_given_valid_params_when_kickoff_then_completion_status_increases_and_fi
     RE(set_fast_grid_scan_params(fast_grid_scan, GridScanParams(3, 3)))
     fast_grid_scan.stage()
     assert fast_grid_scan.position_counter.get() == 0
+
+    # S03 currently is giving 2* the number of expected images (see #13)
+    fast_grid_scan.expected_images.put(3 * 3 * 2)
+
     fast_grid_scan.kickoff()
     complete_status = fast_grid_scan.complete()
     complete_status.watch(progress_watcher)
