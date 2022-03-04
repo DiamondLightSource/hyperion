e4687949c427e06a23f17e51b01419f59fc30bc0
Ben Taylor <benjamin.taylor@diamond.ac.uk>
2022-03-04 10:52:21 +0000

    restructured the classes, added ispyb parameters to be passed in with others

--- src/artemis/devices/det_dist_to_beam_converter.py
+++ src/artemis/devices/det_dist_to_beam_converter.py
@@ -1,29 +1,33 @@
 from numpy import loadtxt, interp
+from enum import Enum
 
 
 class DetectorDistanceToBeamXYConverter:
 	lookup_file: str
 	lookup_table_values: list
 
+	class Axis(Enum):
+		X_AXIS = 1
+		Y_AXIS = 2
+
 	def __init__(self, lookup_file: str):
 		self.lookup_file = lookup_file
 		self.lookup_table_values = self.parse_table()
 
-	def get_beam_xy_from_det_dist_mm(self, det_dist_mm: float, beam_axis_values: list) -> float:
+	def get_beam_xy_from_det_dist_mm(self, det_dist_mm: float, beam_axis: Axis) -> float:
+		beam_axis_values = self.lookup_table_values[1] if beam_axis == Axis.Y_AXIS else self.lookup_table_values[2]
 		det_dist_array = self.lookup_table_values[0]
 		return interp(det_dist_mm, det_dist_array, beam_axis_values)
 
-	def get_beam_axis_pixels(self, det_distance: float, image_size_pixels: int, det_dim: float, beam_axis_values: list) -> float:
+	def get_beam_axis_pixels(self, det_distance: float, image_size_pixels: int, det_dim: float, beam_axis: Axis) -> float:
 		beam_mm = self.get_beam_xy_from_det_dist_mm(det_distance, beam_axis_values)
 		return beam_mm * image_size_pixels / det_dim
 
 	def get_beam_y_pixels(self, det_distance: float, image_size_pixels: int, det_dim: float) -> float:
-		beam_y_values = self.lookup_table_values[1]
-		return self.get_beam_axis_pixels(det_distance, image_size_pixels, det_dim, beam_y_values)
+		return self.get_beam_axis_pixels(det_distance, image_size_pixels, det_dim, Axis.Y_AXIS)
 
 	def get_beam_x_pixels(self, det_distance: float, image_size_pixels: int, det_dim: float) -> float:
-		beam_x_values = self.lookup_table_values[2]
-		return self.get_beam_axis_pixels(det_distance, image_size_pixels, det_dim, beam_x_values)
+		return self.get_beam_axis_pixels(det_distance, image_size_pixels, det_dim, Axis.X_AXIS)
 
 	def reload_lookup_table(self):
 		self.lookup_table_values = self.parse_table()
--- src/artemis/ispyb/store_in_ispyb.py
+++ src/artemis/ispyb/store_in_ispyb.py
@@ -1,133 +1,186 @@
 import ispyb
+import datetime
+import re
 from sqlalchemy.connectors import Connector
+from dataclasses import dataclass
+from enum import Enum
 
-from src.artemis.ispyb.ispyb_dataclasses import GridInfo, DataCollection, DataCollectionGroup, Position
+from src.artemis.fast_grid_scan_plan import FullParameters
+from src.artemis.devices.det_dist_to_beam_converter import DetectorDistanceToBeamXYConverter
 
 
-ISPYB_CONFIG_FILE = "/dls_sw/dasc/mariadb/credentials/ispyb-dev.cfg"
+@dataclass
+class IspybParams:
+    sample_id: int
+    visit_path: str
+    undulator_gap: float
+    pixels_per_micron_x: float
+    pixels_per_micron_y: float
+    upper_left: list
+    bottom_right: list
+    sample_barcode: str
+    position: list
+    synchrotron_mode: str
+    xtal_snapshots: str
+    run_number: int
+    transmission: float
+    flux: float
+    wavelength: float
+    beam_size_x: float
+    beam_size_y: float
+    slit_gap_size_x: float
+    slit_gap_size_y: float
+    focal_spot_size_x: float
+    focal_spot_size_y: float
+    comment: str
+    resolution: float
+
+
+class Orientation(Enum):
+    HORIZONTAL = "horizontal"
+    VERTICAL = "vertical"
+
+
+I03_EIGER_DETECTOR = 78
+EIGER_FILE_SUFFIX = "h5"
 
 
 class StoreInIspyb:
 
-    conn: Connector
+    VISIT_PATH_REGEX = r".+/([a-zA-Z]{2}\d{4,5}-\d{1,3})/"
 
-    def store_grid_scan(self, grid_info: GridInfo, data_collection: DataCollection, position: Position,
-                        data_collection_group: DataCollectionGroup):
+    def __init__(self, ispyb_config, full_params: FullParameters):
+        self.ISPYB_CONFIG_FILE = ispyb_config
+        self.full_params = full_params
+        self.conn: Connector = None
+        self.mx_acquisition = None
+        self.core = None
 
-        with ispyb.open(ISPYB_CONFIG_FILE) as self.conn:
-            data_collection.data_collection_group_id = self.store_data_collection_group_table(data_collection_group)
-            data_collection.position_id = self.store_position_table(position)
+    def store_grid_scan(self):
 
-            data_collection_id = self.store_data_collection_table(data_collection)
+        with ispyb.open(self.ISPYB_CONFIG_FILE) as self.conn:
+            self.mx_acquisition = self.conn.mx_acquisition
+            self.core = self.conn.core
 
-            grid_info.ispyb_data_collection_id = data_collection_id
+            data_collection_group_id = self.__store_data_collection_group_table()
+            position_id = self.__store_position_table()
 
-            grid_id = self.store_grid_info_table(grid_info)
+            data_collection_id = self.__store_data_collection_table(position_id, data_collection_group_id)
+
+            grid_id = self.__store_grid_info_table(data_collection_id)
 
             return grid_id, data_collection_id
 
     def update_grid_scan_with_end_time_and_status(self, end_time: str, run_status: str, dc_id: int) -> int:
-        with ispyb.open(ISPYB_CONFIG_FILE) as self.conn:
-            mx_acquisition = self.conn.mx_acquisition
+        with ispyb.open(self.ISPYB_CONFIG_FILE) as self.conn:
+            self.mx_acquisition = self.conn.mx_acquisition
 
-            params = mx_acquisition.get_data_collection_params()
+            params = self.mx_acquisition.get_data_collection_params()
             params["id"] = dc_id
             params["endtime"] = end_time
             params["run_status"] = run_status
 
-            return mx_acquisition.upsert_data_collection(list(params.values()))
+            return self.mx_acquisition.upsert_data_collection(list(params.values()))
 
-    def store_grid_info_table(self, grid_info: GridInfo) -> int:
-        mx_acquisition = self.conn.mx_acquisition
+    def __store_grid_info_table(self, ispyb_data_collection_id: int) -> int:
+        params = self.mx_acquisition.get_dc_grid_params()
 
-        params = mx_acquisition.get_dc_grid_params()
+        params["parentid"] = ispyb_data_collection_id
+        params["dxInMm"] = self.full_params.grid_scan_params.x_step_size
+        params["dyInMm"] = self.full_params.grid_scan_params.y_step_size
+        params["stepsX"] = self.full_params.grid_scan_params.x_steps
+        params["stepsY"] = self.full_params.grid_scan_params.y_steps
+        params["pixelsPerMicronX"] = self.full_params.ispyb_params.pixels_per_micron_x
+        params["pixelsPerMicronY"] = self.full_params.ispyb_params.pixels_per_micron_y
+        params["snapshotOffsetXPixel"], params["snapshotOffsetYPixel"] = self.full_params.ispyb_params.upper_left
+        params["orientation"] = Orientation.HORIZONTAL.value
+        params["snaked"] = True
 
-        params["parentid"] = grid_info.ispyb_data_collection_id
-        params["dxInMm"] = grid_info.dx_mm
-        params["dyInMm"] = grid_info.dy_mm
-        params["stepsX"] = grid_info.steps_x
-        params["stepsY"] = grid_info.steps_y
-        params["pixelsPerMicronX"] = grid_info.pixels_per_micron_x
-        params["pixelsPerMicronY"] = grid_info.pixels_per_micron_y
-        params["snapshotOffsetXPixel"] = grid_info.snapshot_offset_pixel_x
-        params["snapshotOffsetYPixel"] = grid_info.snapshot_offset_pixel_y
-        params["orientation"] = grid_info.orientation
-        params["snaked"] = grid_info.snaked
+        return self.mx_acquisition.upsert_dc_grid(list(params.values()))
 
-        return mx_acquisition.upsert_dc_grid(list(params.values()))
+    def __store_data_collection_table(self, position_id: int, data_collection_group_id: int) -> int:
+        session_id = self.core.retrieve_visit_id(
+            self.get_visit_string_from_visit_path(
+                self.full_params.ispyb_params.visit_path))
 
-    def store_data_collection_table(self, data_collection: DataCollection) -> int:
-        core = self.conn.core
-        mx_acquisition = self.conn.mx_acquisition
+        params = self.mx_acquisition.get_data_collection_params()
+        params["visitid"] = session_id
+        params["parentid"] = data_collection_group_id
+        params["positonid"] = position_id
+        params["sampleid"] = self.full_params.ispyb_params.sample_id
+        params["detectorid"] = I03_EIGER_DETECTOR
+        params["axis_start"] = self.full_params.detector_params.omega_start
+        params["axis_end"] = 0
+        params["axis_range"] = 0
+        params["focal_spot_size_at_samplex"] = self.full_params.ispyb_params.focal_spot_size_x
+        params["focal_spot_size_at_sampley"] = self.full_params.ispyb_params.focal_spot_size_y
+        params["slitgap_vertical"] = self.full_params.ispyb_params.slit_gap_size_y
+        params["slitgap_horizontal"] = self.full_params.ispyb_params.slit_gap_size_x
+        params["beamsize_at_samplex"] = self.full_params.ispyb_params.beam_size_x
+        params["beamsize_at_sampley"] = self.full_params.ispyb_params.beam_size_y
+        params["transmission"] = self.full_params.ispyb_params.transmission
+        params["comments"] = self.full_params.ispyb_params.comment
+        params["datacollection_number"] = self.full_params.ispyb_params.run_number
+        params["detector_distance"] = self.full_params.detector_params.detector_distance
+        params["exp_time"] = self.full_params.detector_params.exposure_time
+        params["imgdir"] = self.full_params.detector_params.directory
+        params["imgprefix"] = self.full_params.detector_params.prefix
+        params["imgsuffix"] = EIGER_FILE_SUFFIX
+        params["n_images"] = self.full_params.detector_params.num_images
+        params["n_passes"] = 1
+        params["overlap"] = 0
+        params["flux"] = self.full_params.ispyb_params.flux
+        params["omegastart"] = self.full_params.detector_params.omega_start
+        params["start_image_number"] = 1
+        params["resolution"] = self.full_params.ispyb_params.resolution
+        params["wavelength"] = self.full_params.ispyb_params.wavelength
+        params["xbeam"],
+        params["ybeam"] = self.get_beam_position_mm(self.full_params.detector_params.detector_distance)
+        params["xtal_snapshot1"],
+        params["xtal_snapshot2"],
+        params["xtal_snapshot3"] = self.full_params.ispyb_params.xtal_snapshots * 3
+        params["synchrotron_mode"] = self.full_params.ispyb_params.synchrotron_mode
+        params["undulator_gap1"] = self.full_params.ispyb_params.undulator_gap
+        params["starttime"] = self.get_current_time_string()
+        params["file_template"] # same as master h5 file name
 
-        sessionid = core.retrieve_visit_id(data_collection.visit)
+        return self.mx_acquisition.upsert_data_collection(list(params.values()))
 
-        params = mx_acquisition.get_data_collection_params()
-        params["visitid"] = sessionid
-        params["parentid"] = data_collection.data_collection_group_id
-        params["positonid"] = data_collection.position_id
-        params["sampleid"] = data_collection.sample_id
-        params["detectorid"] = data_collection.detector_id
-        params["axis_start"] = data_collection.axis_start
-        params["axis_end"] = data_collection.axis_end
-        params["axis_range"] = data_collection.axis_range
-        params["focal_spot_size_at_samplex"] = data_collection.focal_spot_size_at_sample_x
-        params["focal_spot_size_at_sampley"] = data_collection.focal_spot_size_at_sample_y
-        params["slitgap_vertical"] = data_collection.slitgap_vertical
-        params["slitgap_horizontal"] = data_collection.slitgap_horizontal
-        params["beamsize_at_samplex"] = data_collection.beamsize_at_sample_x
-        params["beamsize_at_sampley"] = data_collection.beamsize_at_sample_y
-        params["transmission"] = data_collection.transmission
-        params["comments"] = data_collection.comments
-        params["datacollection_number"] = data_collection.data_collection_number
-        params["detector_distance"] = data_collection.detector_distance
-        params["exp_time"] = data_collection.exposure_time
-        params["imgdir"] = data_collection.img_dir
-        params["imgprefix"] = data_collection.img_prefix
-        params["imgsuffix"] = data_collection.img_suffix
-        params["n_images"] = data_collection.number_of_images
-        params["n_passes"] = data_collection.number_of_passes
-        params["overlap"] = data_collection.overlap
-        params["flux"] = data_collection.flux
-        params["omegastart"] = data_collection.omega_start
-        params["start_image_number"] = data_collection.start_image_number
-        params["resolution"] = data_collection.resolution
-        params["wavelength"] = data_collection.wavelength
-        params["xbeam"] = data_collection.x_beam
-        params["ybeam"] = data_collection.y_beam
-        params["xtal_snapshot1"] = data_collection.xtal_snapshots_1
-        params["xtal_snapshot2"] = data_collection.xtal_snapshots_2
-        params["xtal_snapshot3"] = data_collection.xtal_snapshots_3
-        params["synchrotron_mode"] = data_collection.synchrotron_mode
-        params["undulator_gap1"] = data_collection.undulator_gap
-        params["starttime"] = data_collection.start_time
-        params["endtime"] = data_collection.end_time
-        params["run_status"] = data_collection.run_status
-        params["file_template"] = data_collection.file_template
-        params["binning"] = data_collection.binning
+    def __store_position_table(self) -> int:
+        params = self.mx_acquisition.get_dc_position_params()
 
-        return mx_acquisition.upsert_data_collection(list(params.values()))
+        params["pos_x"], params["pos_y"], params["pos_z"] = self.full_params.ispyb_params.position
 
-    def store_position_table(self, position: Position) -> int:
-        mx_acquisition = self.conn.mx_acquisition
+        return self.mx_acquisition.update_dc_position(list(params.values()))
 
-        params = mx_acquisition.get_dc_position_params()
-        params["pos_x"] = position.position_x
-        params["pos_y"] = position.position_y
-        params["pos_z"] = position.position_z
+    def __store_data_collection_group_table(self) -> int:
+        session_id = self.core.retrieve_visit_id(
+            self.get_visit_string_from_visit_path(
+                self.full_params.ispyb_params.visit_path))
 
-        return mx_acquisition.update_dc_position(list(params.values()))
+        params = self.mx_acquisition.get_data_collection_params()
+        params["parentid"] = session_id
+        params["experimenttype"] = "mesh"
+        params["sampleid"] = self.full_params.ispyb_params.sample_id
+        params["sample_barcode"] = self.full_params.ispyb_params.sample_barcode
 
-    def store_data_collection_group_table(self, data_collection_group: DataCollectionGroup) -> int:
-        core = self.conn.core
-        mx_acquisition = self.conn.mx_acquisition
+        return self.mx_acquisition.upsert_data_collection_group(list(params.values()))
 
-        sessionid = core.retrieve_visit_id(data_collection_group.visit)
+    def get_current_time_string(self):
+        now = datetime.datetime.now()
+        return now.strftime("%Y/%m/%d %H:%M:%S")
 
-        params = mx_acquisition.get_data_collection_params()
-        params["parentid"] = sessionid
-        params["experimenttype"] = data_collection_group.experimenttype
-        params["sampleid"] = data_collection_group.sample_id
-        params["sample_barcode"] = data_collection_group.sample_barcode
+    def get_visit_string_from_visit_path(self, visit_path):
+        return re.search(self.VISIT_PATH_REGEX, visit_path).group(1)
 
-        return mx_acquisition.upsert_data_collection_group(list(params.values()))
+    def get_beam_position_mm(self, detector_distance): # possibly should be in beam_xy_converter
+        x_beam_mm = self.full_params.det_to_distance.get_beam_xy_from_det_dist_mm(detector_distance, DetectorDistanceToBeamXYConverter.Axis.X_AXIS)
+        y_beam_mm = self.full_params.det_to_distance.get_beam_xy_from_det_dist_mm(detector_distance, DetectorDistanceToBeamXYConverter.Axis.Y_AXIS)
+
+        full_size_mm = self.full_params.detector.det_dimension
+        roi_size_mm = self.full_params.detector.roi_dimension
+
+        offset_x = (full_size_mm.width - roi_size_mm.width) / 2.
+        offset_y = (full_size_mm.height - roi_size_mm.height) / 2.
+
+        return x_beam_mm - offset_x, y_beam_mm - offset_y
