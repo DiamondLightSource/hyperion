9be1775d6e124e26cbc81ab40ca7a13f6df742b1
Dominic Oram <dominic.oram@diamond.ac.uk>
2022-04-12 12:13:09 +0100

    Call ispyb and zocalo as part of grid scan

--- src/artemis/fast_grid_scan_plan.py
+++ src/artemis/fast_grid_scan_plan.py
@@ -1,31 +1,29 @@
-from collections import namedtuple
 import os
 import sys
-
-from pydantic import NoneIsNotAllowedError
+from collections import namedtuple
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
+import argparse
 from dataclasses import dataclass
+
+import bluesky.plan_stubs as bps
+import bluesky.preprocessors as bpp
+from bluesky import RunEngine
+from bluesky.log import config_bluesky_logging
+from bluesky.utils import ProgressBarManager
+from dataclasses_json import dataclass_json
+from ophyd.log import config_ophyd_logging
 from src.artemis.devices.eiger import DetectorParams, EigerDetector
-from src.artemis.ispyb.ispyb_dataclass import IspybParams, Point2D, Point3D
 from src.artemis.devices.fast_grid_scan import (
     FastGridScan,
     GridScanParams,
     set_fast_grid_scan_params,
 )
-import bluesky.preprocessors as bpp
-import bluesky.plan_stubs as bps
-from bluesky import RunEngine
-from bluesky.utils import ProgressBarManager
-
 from src.artemis.devices.zebra import Zebra
-import argparse
-
-from ophyd.log import config_ophyd_logging
-from bluesky.log import config_bluesky_logging
-
-from dataclasses_json import dataclass_json
+from src.artemis.ispyb.ispyb_dataclass import IspybParams, Point2D, Point3D
+from src.artemis.ispyb.store_in_ispyb import StoreInIspyb
+from src.artemis.zocalo_interaction import run_end, run_start
 
 config_bluesky_logging(file="/tmp/bluesky.log", level="DEBUG")
 config_ophyd_logging(file="/tmp/ophyd.log", level="DEBUG")
@@ -95,6 +93,9 @@ class FullParameters:
 def run_gridscan(
     fgs: FastGridScan, zebra: Zebra, eiger: EigerDetector, parameters: FullParameters
 ):
+    ispyb = StoreInIspyb("config", parameters)
+    grid_id, dc_id = ispyb.store_grid_scan()
+    run_start(dc_id)
     # TODO: Check topup gate
     yield from set_fast_grid_scan_params(fgs, parameters.grid_scan_params)
 
@@ -104,6 +105,12 @@ def run_gridscan(
         yield from bps.complete(fgs, wait=True)
 
     yield from do_fgs()
+
+    current_time = ispyb.get_current_time_string()
+    ispyb.update_grid_scan_with_end_time_and_status(
+        current_time, "DataCollection Successful", dc_id
+    )
+    run_end(dc_id)
 
 
 def get_plan(parameters: FullParameters):
