3f3a278e4ba8facce54d2b3cb797bdd31b32a2b8
Dominic Oram <dominic.oram@diamond.ac.uk>
2022-05-24 18:21:39 +0100

    97: Added function to wait for analysis results

--- src/artemis/zocalo_interaction.py
+++ src/artemis/zocalo_interaction.py
@@ -1,16 +1,29 @@
 import getpass
 import socket
+from time import sleep
 
+import workflows.recipe
+import workflows.transport
 import zocalo.configuration
-from workflows.transport import default_transport, lookup
+from src.artemis.ispyb.ispyb_dataclass import Point3D
+from workflows.transport import lookup
+
+TIMEOUT = 30
 
 
-def _send_to_zocalo(parameters: dict):
+def _get_zocalo_connection():
     zc = zocalo.configuration.from_file()
     zc.activate()
 
     transport = lookup("PikaTransport")()
     transport.connect()
+
+    return transport
+
+
+def _send_to_zocalo(parameters: dict):
+    transport = _get_zocalo_connection()
+
     try:
         message = {
             "recipes": ["mimas"],
@@ -49,3 +62,47 @@ def run_end(data_collection_id: int):
             "ispyb_dcid": data_collection_id,
         }
     )
+
+
+def wait_for_result(data_collection_id: int, timeout: int = TIMEOUT) -> Point3D:
+    """Block until a result is recieved from Zocalo.
+    Args:
+        data_collection_id (int): The ID of the data collection representing the gridscan in ISpyB
+        timeout (float): The time in seconds to wait for the result to be recieved.
+    Returns:
+        Point in grid co-ordinates that is the centre point to move to
+    """
+    transport = _get_zocalo_connection()
+    result_recieved = None
+
+    def receive_result(
+        rw: workflows.recipe.RecipeWrapper, header: dict, message: dict
+    ) -> None:
+        print(f"Received {message}")
+        recipe_parameters = rw.recipe_step["parameters"]
+        print(f"Recipe step parameters: {recipe_parameters}")
+        if recipe_parameters["dcid"] == str(data_collection_id):
+            transport.ack(header)
+            nonlocal result_recieved
+            result_recieved = Point3D(*message["max_voxel"])
+        else:
+            print(
+                f"Warning: results for {recipe_parameters['dcid']} recieved but expected {data_collection_id}"
+            )
+
+    workflows.recipe.wrap_subscribe(
+        transport,
+        "xrc.i03",
+        receive_result,
+        acknowledgement=True,
+        allow_non_recipe_messages=True,
+    )
+
+    try:
+        for _ in range(timeout):
+            sleep(1)
+            if result_recieved:
+                return result_recieved
+        raise TimeoutError(f"Result not received in {timeout} seconds")
+    finally:
+        transport.disconnect()
