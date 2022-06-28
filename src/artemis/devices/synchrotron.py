aa07b47980e1197a716521884f7056a509e202e5
Abi Emery <abigail.emery@diamond.ac.uk>
2022-06-28 16:00:11 +0100

    #126:Tidy up synchrotron device

--- src/artemis/devices/synchrotron.py
+++ src/artemis/devices/synchrotron.py
@@ -2,27 +2,21 @@ from ophyd import Component, Device, EpicsSignal
 
 
 class SynchrotoronMachineStatus(Device):
-    synchrotron_mode: EpicsSignal = Component(
-        EpicsSignal, "CS-CS-MSTAT-01:MODE", string=True
-    )
-    ring_energy: EpicsSignal = Component(EpicsSignal, "CS-CS-MSTAT-01:BEAMENERGY")
-    beam_dump_countdown: EpicsSignal = Component(
-        EpicsSignal, "CS-CS-MSTAT-01:USERCOUNTDN"
-    )
+    synchrotron_mode: EpicsSignal = Component(EpicsSignal, "MODE", string=True)
+    beam_dump_countdown: EpicsSignal = Component(EpicsSignal, "USERCOUNTDN")
+    ring_energy: EpicsSignal = Component(EpicsSignal, "BEAMENERGY")
 
 
 class SynchrotronTopUp(Device):
-    topup_start_countdown: EpicsSignal = Component(
-        EpicsSignal, "SR-CS-FILL-01:COUNTDOWN"
-    )
-    topup_end_countdown: EpicsSignal = Component(
-        EpicsSignal, "SR-CS-FILL-01:ENDCOUNTDN"
-    )
+    topup_start_countdown: EpicsSignal = Component(EpicsSignal, "COUNTDOWN")
+    topup_end_countdown: EpicsSignal = Component(EpicsSignal, "ENDCOUNTDN")
 
 
 class Synchrotron(Device):
 
-    machine_status: SynchrotoronMachineStatus = Component(SynchrotoronMachineStatus, "")
-    top_up: SynchrotronTopUp = Component(SynchrotronTopUp, "")
+    machine_status: SynchrotoronMachineStatus = Component(
+        SynchrotoronMachineStatus, "CS-CS-MSTAT-01:"
+    )
+    top_up: SynchrotronTopUp = Component(SynchrotronTopUp, "SR-CS-FILL-01:")
 
     ring_current: EpicsSignal = Component(EpicsSignal, "SR-DI-DCCT-01:SIGNAL")
