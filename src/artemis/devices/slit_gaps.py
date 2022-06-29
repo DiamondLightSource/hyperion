8e80892dfcce26ebc57f38ae448abd23bf284401
Abi Emery <abigail.emery@diamond.ac.uk>
2022-06-29 11:13:56 +0100

    #140: Update slit gap pv

--- src/artemis/devices/slit_gaps.py
+++ src/artemis/devices/slit_gaps.py
@@ -3,5 +3,5 @@ from ophyd import Component, Device, EpicsSignal
 
 class SlitGaps(Device):
 
-    xgap: EpicsSignal = Component(EpicsSignal, "BL03I-AL-SLITS-04:XGAP")
-    ygap: EpicsSignal = Component(EpicsSignal, "BL03I-AL-SLITS-04:YGAP")
+    xgap: EpicsSignal = Component(EpicsSignal, "XGAP")
+    ygap: EpicsSignal = Component(EpicsSignal, "YGAP")
