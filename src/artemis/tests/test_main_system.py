d923afec95cca4083ca983e9aca3440a32252e02
Dominic Oram <dominic.oram@diamond.ac.uk>
2022-07-15 16:01:40 +0100

    (#165) Used patched get_plan in tests

--- src/artemis/tests/test_main_system.py
+++ src/artemis/tests/test_main_system.py
@@ -3,11 +3,12 @@ import threading
 from dataclasses import dataclass
 from time import sleep
 from typing import Any, Callable
+from unittest.mock import patch
 
 import pytest
 from flask.testing import FlaskClient
-from src.artemis.devices.det_dim_constants import EIGER_TYPE_EIGER2_X_4M
-from src.artemis.main import Actions, Status, StatusAndMessage, create_app
+
+from src.artemis.main import Actions, Status, create_app
 from src.artemis.parameters import FullParameters
 
 FGS_ENDPOINT = "/fast_grid_scan/"
@@ -49,7 +50,10 @@ def test_env():
     app, runner = create_app({"TESTING": True}, mock_run_engine)
     runner_thread = threading.Thread(target=runner.wait_on_queue)
     runner_thread.start()
-    with app.test_client() as client:
+    with (
+        app.test_client() as client,
+        patch("src.artemis.main.get_plan") as _,
+    ):
         yield ClientAndRunEngine(client, mock_run_engine)
 
     runner.shutdown()
