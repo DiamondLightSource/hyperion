6159f446710a502fce361d9f5e6dd2cb648f579a
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-07-19 10:14:16 +0100

    #128: Refactor Snapshot to allow for SnapshotWithGrid to inherit

--- src/artemis/devices/oav.py
+++ src/artemis/devices/oav.py
@@ -1,4 +1,5 @@
 import threading
+from pathlib import Path
 
 import requests
 from ophyd import ADComponent as ADC
@@ -20,6 +21,7 @@ from PIL import Image
 
 class Snapshot(Device):
     filename: Signal = Component(Signal)
+    directory: Signal = Component(Signal)
     url: EpicsSignal = Component(EpicsSignal, ":JPG_URL_RBV", string=True)
     KICKOFF_TIMEOUT: float = 10.0
 
@@ -27,13 +29,16 @@ class Snapshot(Device):
         st = DeviceStatus(device=self, timeout=self.KICKOFF_TIMEOUT)
         url_str = self.url.get()
         filename_str = self.filename.get()
+        directory_str = self.directory.get()
 
         def get_snapshot():
             try:
                 response = requests.get(url_str, stream=True)
                 response.raise_for_status()
                 image = Image.open(response.raw)
-                image.save(filename_str)
+                image_path = Path(f"{directory_str}/{filename_str}.png")
+                image.save(image_path)
+                self.post_processing(image)
                 st.set_finished()
             except requests.HTTPError as e:
                 st.set_exception(e)
@@ -41,6 +46,9 @@ class Snapshot(Device):
         threading.Thread(target=get_snapshot, daemon=True).start()
 
         return st
+
+    def post_processing(self, image: Image.Image):
+        pass
 
 
 class OAV(AreaDetector):
--- src/artemis/devices/unit_tests/test_oav.py
+++ src/artemis/devices/unit_tests/test_oav.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 import PIL
@@ -15,6 +16,7 @@ def fake_oav() -> OAV:
 
     fake_oav.snapshot.url.sim_put("http://test.url")
     fake_oav.snapshot.filename.put("test filename")
+    fake_oav.snapshot.directory.put("test directory")
 
     return fake_oav
 
@@ -51,4 +53,5 @@ def test_snapshot_trigger_saves_to_correct_file(
     mock_open.return_value = image
     st = fake_oav.snapshot.trigger()
     st.wait()
-    mock_save.assert_called_once_with("test filename")
+    expected_path = Path("test directory") / "test filename.png"
+    mock_save.assert_called_once_with(expected_path)
