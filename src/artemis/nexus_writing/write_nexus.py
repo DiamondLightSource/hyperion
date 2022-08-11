cf65e697777184279e043b695562f6efc1f1ebdb
Ben Taylor <benjamin.taylor@diamond.ac.uk>
2022-08-11 17:04:48 +0100

    remove comment

--- src/artemis/nexus_writing/write_nexus.py
+++ src/artemis/nexus_writing/write_nexus.py
@@ -104,7 +104,7 @@ def create_detector_parameters(detector_params: DetectorParams) -> Dict:
         "sensor_material": "Silicon",
         "sensor_thickness": "4.5E-4",
         "overload": 46051,
-        "underload": 0,  # sure of this
+        "underload": 0,
         "pixel_size": ["0.075mm", "0.075mm"],
         "flatfield": "flatfield",
         "flatfield_applied": "_dectris/flatfield_correction_applied",
