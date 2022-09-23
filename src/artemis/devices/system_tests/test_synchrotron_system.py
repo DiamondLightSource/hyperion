e9816b671409f760e7c0ae0b5466ce035a99d407
Abi Emery <abigail.emery@diamond.ac.uk>
2022-09-23 10:32:10 +0100

    Leave only the synchrotron connection test

--- src/artemis/devices/system_tests/test_synchrotron_system.py
+++ src/artemis/devices/system_tests/test_synchrotron_system.py
@@ -3,13 +3,6 @@ import pytest
 from artemis.devices.synchrotron import Synchrotron
 from artemis.parameters import SIM_BEAMLINE
 
-SIGNAL = 300
-MODE = "Shutdown"
-USERCOUNTDN = 100000
-BEAMENERGY = 3
-COUNTDOWN = 600
-ENDCOUNTDN = 610
-
 
 @pytest.fixture
 def synchrotron():
@@ -20,13 +13,3 @@ def synchrotron():
 @pytest.mark.s03
 def test_synchrotron_connects(synchrotron: Synchrotron):
     synchrotron.wait_for_connection()
-
-
-@pytest.mark.s03
-def test_get_values(synchrotron: Synchrotron):
-    assert synchrotron.ring_current.get() == SIGNAL
-    assert synchrotron.machine_status.synchrotron_mode.get() == MODE
-    assert synchrotron.machine_status.user_countdown.get() == USERCOUNTDN
-    assert synchrotron.machine_status.beam_energy.get() == BEAMENERGY
-    assert synchrotron.top_up.start_countdown.get() == COUNTDOWN
-    assert synchrotron.top_up.end_countdown.get() == ENDCOUNTDN
