1e3ba3651531c9f5263247ea911780bebef2eefd
Abi Emery <abigail.emery@diamond.ac.uk>
2022-09-26 17:02:16 +0000

    Fix and update tests to correctly mock logging

--- src/artemis/tests/test_log.py
+++ src/artemis/tests/test_log.py
@@ -1,50 +1,76 @@
-import logging
-from importlib import reload
 from os import environ
 from pathlib import Path
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 from artemis import log
 
 
-@pytest.fixture(autouse=True, scope="module")
-def cleanup_logger():
-    yield
-    reload(logging)
-    reload(log)
+@pytest.fixture()
+def mock_logger():
+    with patch("artemis.log.LOGGER") as mock_LOGGER:
+        yield mock_LOGGER
 
 
 @patch("artemis.log.GELFTCPHandler")
 @patch("artemis.log.logging")
-def test_handlers_set_at_correct_debug_level(mock_logging, mock_graypy):
-    log.set_up_logging(None, False)
-    handlers = log.LOGGER.handlers
-    assert len(handlers) == 3
+def test_handlers_set_at_correct_default_level(
+    mock_logging,
+    mock_GELFTCPHandler,
+    mock_logger: MagicMock,
+):
+    handlers = log.set_up_logging_handlers(None, False)
+
     for handler in handlers:
+        mock_logger.addHandler.assert_any_call(handler)
         handler.setLevel.assert_called_once_with("INFO")
 
 
 @patch("artemis.log.GELFTCPHandler")
 @patch("artemis.log.logging")
-def test_dev_mode_sets_correct_graypy_handler(mock_logging, mock_GELFTCPHandler):
-    log.set_up_logging(None, True)
-    mock_GELFTCPHandler.assert_called_once_with("localhost", 5555)
-    pass
+def test_handlers_set_at_correct_debug_level(
+    mock_logging,
+    mock_GELFTCPHandler,
+    mock_logger: MagicMock,
+):
+    handlers = log.set_up_logging_handlers("DEBUG", True)
+
+    for handler in handlers:
+        mock_logger.addHandler.assert_any_call(handler)
+        handler.setLevel.assert_called_once_with("DEBUG")
 
 
 @patch("artemis.log.GELFTCPHandler")
 @patch("artemis.log.logging")
-def test_prod_mode_sets_correct_graypy_handler(mock_logging, mock_GELFTCPHandler):
-    log.set_up_logging(None, False)
+def test_dev_mode_sets_correct_graypy_handler(
+    mock_logging,
+    mock_GELFTCPHandler,
+    mock_logger: MagicMock,
+):
+    log.set_up_logging_handlers(None, True)
+    mock_GELFTCPHandler.assert_called_once_with("localhost", 5555)
+
+
+@patch("artemis.log.GELFTCPHandler")
+@patch("artemis.log.logging")
+def test_prod_mode_sets_correct_graypy_handler(
+    mock_logging,
+    mock_GELFTCPHandler,
+    mock_logger: MagicMock,
+):
+    log.set_up_logging_handlers(None, False)
     mock_GELFTCPHandler.assert_called_once_with("graylog2.diamond.ac.uk", 12218)
 
 
 @patch("artemis.log.GELFTCPHandler")
 @patch("artemis.log.logging")
-def test_dev_mode_sets_correct_file_handler(mock_logging, mock_GELFTCPHandler):
-    log.set_up_logging(None, True)
+def test_dev_mode_sets_correct_file_handler(
+    mock_logging,
+    mock_GELFTCPHandler,
+    mock_logger: MagicMock,
+):
+    log.set_up_logging_handlers(None, True)
     mock_logging.FileHandler.assert_called_once_with(
         filename=Path("./tmp/dev/artemis.txt")
     )
@@ -53,8 +79,28 @@ def test_dev_mode_sets_correct_file_handler(mock_logging, mock_GELFTCPHandler):
 @patch("artemis.log.GELFTCPHandler")
 @patch("artemis.log.logging")
 @patch.dict(environ, {"BEAMLINE": "S03"})
-def test_prod_mode_sets_correct_file_handler(mock_logging, mock_GELFTCPHandler):
-    log.set_up_logging(None, False)
+def test_prod_mode_sets_correct_file_handler(
+    mock_logging,
+    mock_GELFTCPHandler,
+    mock_logger: MagicMock,
+):
+    log.set_up_logging_handlers(None, False)
     mock_logging.FileHandler.assert_called_once_with(
         filename=Path("/dls_sw/S03/logs/bluesky/artemis.txt")
     )
+
+
+@patch("artemis.log.GELFTCPHandler")
+@patch("artemis.log.logging")
+def test_setting_debug_in_prod_gives_warning(
+    mock_logging,
+    mock_GELFTCPHandler,
+    mock_logger: MagicMock,
+):
+    warning_string = (
+        'STARTING ARTEMIS IN DEBUG WITHOUT "--dev" WILL FLOOD PRODUCTION '
+        "GRAYLOG WITH MESSAGES. If you really need debug messages, set up a local "
+        "graylog instead!\n"
+    )
+    log.set_up_logging_handlers("DEBUG", False)
+    mock_logger.warning.assert_any_call(warning_string)
