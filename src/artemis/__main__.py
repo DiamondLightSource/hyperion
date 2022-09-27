1c7b57f0068b90a1b75d4d75abeeb6b1eb856b69
Abigail Emery <abigail.emery@diamond.ac.uk>
2022-09-27 17:13:52 +0100

    27 integrate with graylog (#218)
    * Apply combined artemis ophyd bluesky logs in main
    
    * Add dockerfile and contentpack for local graylog image
    
    * Update README for running local graylog instance
    
    * Enable setting up local graylog with setup script
    
    * Change run command to artemis
    
    * Add inital logging config with cli args
    
    * Update README for new flask port and logging changes
    
    * Change logging formatter
    
    * Make logging default to production graylog and beamline/log filepath
    
    * Update README.md for the production changes
    
    * Fix typing python3.8 compatability issue
    
    * Add inital tests for setup_logging
    
    * Fix logging tests with cleanup fixture
    
    * Remove stray comment
    
    * clean up graylog config returns
    
    * Add defaults to setup logging
    
    * Return flask app port back to default
    
    * Move log level warning into setup_logging
    
    * Fix spelling mistake in readme
    
    * Change logging level type in artemis setup log
    
    * Fix logging level defaults
    
    * Store log handlers in list instead of dict
    
    * Make set up logging return handlers
    
    * Fix and update tests to correctly mock logging
    
    * Change list to imported List to fix 3.8 compatability issue
    
    * Update README with correct flask app ports
    
    * Update logging setup call in main
    
    * Move cli args to function
    
    * Add very basic cli test
    
    * Remove unused imports
    
    * Add artemis prefix to logging containers
    
    * (#27) Move to graylog 4.2 to work on REHL 7
    
    * Re-add artemis prefix to logging containers
    
    Co-authored-by: Dominic Oram <dominic.oram@diamond.ac.uk>

--- src/artemis/__main__.py
+++ src/artemis/__main__.py
@@ -1,5 +1,5 @@
+import argparse
 import atexit
-import logging
 import threading
 from dataclasses import dataclass
 from enum import Enum
@@ -12,10 +12,9 @@ from dataclasses_json import dataclass_json
 from flask import Flask, request
 from flask_restful import Api, Resource
 
+import artemis.log
 from artemis.fast_grid_scan_plan import get_plan
 from artemis.parameters import FullParameters
-
-logger = logging.getLogger(__name__)
 
 
 class Actions(Enum):
@@ -58,7 +57,7 @@ class BlueskyRunner:
         self.RE = RE
 
     def start(self, parameters: FullParameters) -> StatusAndMessage:
-        logger.info(f"Started with parameters: {parameters}")
+        artemis.log.LOGGER.info(f"Started with parameters: {parameters}")
         if (
             self.current_status.status == Status.BUSY.value
             or self.current_status.status == Status.ABORTING.value
@@ -149,7 +148,26 @@ def create_app(
     return app, runner
 
 
+def cli_arg_parse() -> Tuple[Optional[bool], Optional[str]]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dev",
+        action="store_true",
+        help="Use dev options, such as local graylog instances and S03",
+    )
+    parser.add_argument(
+        "--logging-level",
+        type=str,
+        choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"],
+        help="Choose overall logging level, defaults to INFO",
+    )
+    args = parser.parse_args()
+    return args.logging_level, args.dev
+
+
 if __name__ == "__main__":
+    args = cli_arg_parse()
+    artemis.log.set_up_logging_handlers(*args)
     app, runner = create_app()
     atexit.register(runner.shutdown)
     flask_thread = threading.Thread(
---
+++ src/artemis/log.py
@@ -0,0 +1,105 @@
+import logging
+from os import environ
+from pathlib import Path
+from typing import List, Tuple, Union
+
+from bluesky.log import config_bluesky_logging
+from bluesky.log import logger as bluesky_logger
+from graypy import GELFTCPHandler
+from ophyd.log import config_ophyd_logging
+from ophyd.log import logger as ophyd_logger
+
+LOGGER = logging.getLogger("Artemis")
+LOGGER.setLevel(logging.DEBUG)  # default logger to log everything
+ophyd_logger.parent = LOGGER
+bluesky_logger.parent = LOGGER
+
+
+def set_up_logging_handlers(
+    logging_level: Union[str, None] = "INFO", dev_mode: bool = False
+) -> List[logging.Handler]:
+    """Set up the logging level and instances for user chosen level of logging.
+
+    Mode defaults to production and can be switched to dev with the --dev flag on run.
+    """
+    logging_level = logging_level if logging_level else "INFO"
+    file_path = Path(_get_logging_file_path(), "artemis.txt")
+    graylog_host, graylog_port = _get_graylog_configuration(dev_mode)
+    formatter = logging.Formatter(
+        "[%(asctime)s] %(name)s %(module)s %(levelname)s: %(message)s"
+    )
+    handlers: list[logging.Handler] = [
+        GELFTCPHandler(graylog_host, graylog_port),
+        logging.StreamHandler(),
+        logging.FileHandler(filename=file_path),
+    ]
+    for handler in handlers:
+        handler.setFormatter(formatter)
+        handler.setLevel(logging_level)
+        LOGGER.addHandler(handler)
+
+    # for assistance in debugging
+    if dev_mode:
+        set_seperate_ophyd_bluesky_files(
+            logging_level=logging_level, logging_path=_get_logging_file_path()
+        )
+
+    # Warn users if trying to run in prod in debug mode
+    if not dev_mode and logging_level == "DEBUG":
+        LOGGER.warning(
+            'STARTING ARTEMIS IN DEBUG WITHOUT "--dev" WILL FLOOD PRODUCTION GRAYLOG'
+            " WITH MESSAGES. If you really need debug messages, set up a"
+            " local graylog instead!\n"
+        )
+
+    return handlers
+
+
+def _get_graylog_configuration(dev_mode: bool) -> Tuple[str, int]:
+    """Get the host and port for the  graylog interaction.
+
+    If running on dev mode, this switches to localhost. Otherwise it publishes to the
+    dls graylog.
+
+    Returns:
+        (host,port): A tuple of the relevent host and port for graylog.
+    """
+    if dev_mode:
+        return "localhost", 5555
+    else:
+        return "graylog2.diamond.ac.uk", 12218
+
+
+def _get_logging_file_path() -> Path:
+    """Get the path to write the artemis log files to.
+
+    If on a beamline, this will be written to the according area depending on the
+    BEAMLINE envrionment variable. If no envrionment variable is found it will default
+    it to the tmp/dev directory.
+
+    Returns:
+        logging_path (Path): Path to the log file for the file handler to write to.
+    """
+    beamline: Union[str, None] = environ.get("BEAMLINE")
+    logging_path: Path
+
+    if beamline:
+        logging_path = Path("/dls_sw/" + beamline + "/logs/bluesky/")
+    else:
+        logging_path = Path("./tmp/dev/")
+        Path(logging_path).mkdir(parents=True, exist_ok=True)
+
+    return logging_path
+
+
+def set_seperate_ophyd_bluesky_files(logging_level: str, logging_path: Path) -> None:
+    """Set file path for the file handlder to the individual Bluesky and Ophyd loggers.
+
+    These provide seperate, nicely formatted logs in the same dir as the artemis log
+    file for each individual module.
+    """
+    bluesky_file_path: Path = Path(logging_path, "bluesky.log")
+    ophyd_file_path: Path = Path(logging_path, "ophyd.log")
+
+    config_bluesky_logging(file=str(bluesky_file_path), level=logging_level)
+    config_ophyd_logging(file=str(ophyd_file_path), level=logging_level)
