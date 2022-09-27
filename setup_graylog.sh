14246c66a2a27673ec662378529bef67698b54ea
Abi Emery <abigail.emery@diamond.ac.uk>
2022-09-27 12:59:08 +0000

    Add artemis prefix to logging containers

--- setup_graylog.sh
+++ setup_graylog.sh
@@ -2,6 +2,6 @@
 
 podman build ./graylog --format docker -t graylog:test
 
-podman run -d --net host --pod=graylog-pod --name=mongo mongo:4.2
-podman run -d --net host --pod=graylog-pod -e "http.host=0.0.0.0" -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" --name=elasticsearch docker.elastic.co/elasticsearch/elasticsearch-oss:7.10.0
-podman run -d --net host --pod=graylog-pod -e GRAYLOG_HTTP_EXTERNAL_URI="http://localhost:9000/" -e GRAYLOG_MONGODB_URI="mongodb://localhost:27017/graylog" -e GRAYLOG_ELASTICSEARCH_HOSTS="http://localhost:9200/" --name=graylog localhost/graylog:test
+podman run -d --net host --pod=graylog-pod --name=artemis-mongo mongo:4.2
+podman run -d --net host --pod=graylog-pod -e "http.host=0.0.0.0" -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" --name=artemis-elasticsearch docker.elastic.co/elasticsearch/elasticsearch-oss:7.10.0
+podman run -d --net host --pod=graylog-pod -e GRAYLOG_HTTP_EXTERNAL_URI="http://localhost:9000/" -e GRAYLOG_MONGODB_URI="mongodb://localhost:27017/graylog" -e GRAYLOG_ELASTICSEARCH_HOSTS="http://localhost:9200/" --name=artemis-graylog localhost/graylog:test
