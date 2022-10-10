52401e89a2659481c77b46b0c2c9da0d18b5955f
David Perl <david.perl@diamond.ac.uk>
2022-10-10 15:15:47 +0000

    remove outdated comments

--- src/artemis/devices/oav/utils.py
+++ src/artemis/devices/oav/utils.py
@@ -1,10 +1,5 @@
 from artemis.utils import Point2D
 
-# def px_from_motor_position(offset:Point2D, pix_per_um_x:float, pix_per_um_y:float) -> Point2D:
-
-# def motor_position_from_px(offset:Point2D, pix_per_um_x:float, pix_per_um_y:float)
-# more complicated, need to know axes
-
 
 def bottom_right_from_top_left(
     top_left: Point2D,
@@ -20,16 +15,3 @@ def bottom_right_from_top_left(
         int(steps_x * step_size_x * 1000 * pix_per_um_x + top_left.x),
         int(steps_y * step_size_y * 1000 * pix_per_um_y + top_left.y),
     )
-
-
-#    def test_bottom_right_from_top_left(dummy_ispyb):
-#
-#  top_left = Point2D(123, 123)
-# bottom_right = dummy_ispyb.bottom_right_from_top_left(
-#        top_left, 20, 30, 0.1, 0.15, 0.37, 0.37
-#   )
-#  assert bottom_right.x == 863 and bottom_right.y == 1788
-# bottom_right = dummy_ispyb.bottom_right_from_top_left(
-#        top_left, 15, 20, 0.005, 0.007, 1, 1
-#   )
-#  assert bottom_right.x == 198 and bottom_right.y == 263
