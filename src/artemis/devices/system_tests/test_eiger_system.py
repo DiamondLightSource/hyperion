f2a5d3d555fa30fffd6cd38027bcba324c3afb50
Eva Lott <evalott100@gmail.com>
2022-10-20 13:27:21 +0100

    Incorrect assert statement removed #225

--- src/artemis/devices/system_tests/test_eiger_system.py
+++ src/artemis/devices/system_tests/test_eiger_system.py
@@ -16,6 +16,8 @@ def eiger():
         omega_increment=0.1,
         num_images=50,
         use_roi_mode=False,
+        run_number=0,
+        det_dist_to_beam_converter_path="src/artemis/devices/unit_tests/test_lookup_table.txt",
     )
     eiger = EigerDetector(
         detector_params=detector_params, name="eiger", prefix="BL03S-EA-EIGER-01:"
@@ -41,9 +43,6 @@ def test_can_stage_and_unstage_eiger(eiger: EigerDetector):
 
     eiger.odin.file_writer.id.subscribe(file_writer_id_monitor)
     eiger.stage()
-    assert (
-        times_id_has_changed == 2
-    )  # Once for initial connection and once for changing the value
     assert eiger.cam.acquire.get() == 1
     # S03 filewriters stay in error
     eiger.odin.check_odin_initialised = lambda: (True, "")
