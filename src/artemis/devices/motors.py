adadb9bb43dd165f92c3fd47945e600691207b4c
David Perl <david.perl@diamond.ac.uk>
2022-11-02 15:50:16 +0000

    work on somewhat unsuccessful communicator test

--- src/artemis/devices/motors.py
+++ src/artemis/devices/motors.py
@@ -43,7 +43,7 @@ class I03Smargon(MotorBundle):
     x: EpicsMotor = Component(EpicsMotor, "X")
     y: EpicsMotor = Component(EpicsMotor, "Y")
     z: EpicsMotor = Component(EpicsMotor, "Z")
-    omega: EpicsMotor = Component(EpicsMotor, "OMEGA")
+    omega: EpicsMotor = Component(EpicsMotor, "Omega")
 
     def get_xyz_limits(self) -> XYZLimitBundle:
         """Get the limits for the x, y and z axes.
--- src/artemis/tests/test_fgs_communicator.py
+++ src/artemis/tests/test_fgs_communicator.py
@@ -4,9 +4,11 @@ from unittest.mock import MagicMock, call, patch
 import pytest
 from bluesky.run_engine import RunEngine
 
-from artemis.fast_grid_scan_plan import get_plan
+from artemis.devices.eiger import EigerDetector
+from artemis.devices.fast_grid_scan_composite import FGSComposite
+from artemis.fast_grid_scan_plan import run_gridscan_and_move
 from artemis.fgs_communicator import FGSCommunicator
-from artemis.parameters import SIM_BEAMLINE, FullParameters
+from artemis.parameters import SIM_BEAMLINE, DetectorParams, FullParameters
 from artemis.plan_names import PLAN_NAMES
 from artemis.utils import Point3D
 
@@ -237,6 +239,36 @@ def test_writers_do_create_one_file_each_on_start_doc(
     assert communicator.nxs_writer_2.create_nexus_file.call_count == 1
 
 
+@pytest.fixture()
+def eiger():
+    detector_params: DetectorParams = DetectorParams(
+        current_energy=100,
+        exposure_time=0.1,
+        directory="/tmp",
+        prefix="file_name",
+        detector_distance=100.0,
+        omega_start=0.0,
+        omega_increment=0.1,
+        num_images=50,
+        use_roi_mode=False,
+        run_number=0,
+        det_dist_to_beam_converter_path="src/artemis/devices/unit_tests/test_lookup_table.txt",
+    )
+    eiger = EigerDetector(
+        detector_params=detector_params, name="eiger", prefix="BL03S-EA-EIGER-01:"
+    )
+
+    # Otherwise odin moves too fast to be tested
+    eiger.cam.manual_trigger.put("Yes")
+
+    # S03 currently does not have StaleParameters_RBV
+    eiger.wait_for_stale_parameters = lambda: None
+    eiger.odin.check_odin_initialised = lambda: (True, "")
+
+    yield eiger
+
+
+@pytest.mark.skip(reason="Needs better S03 or some other workaround.")
 @pytest.mark.s03
 @patch("artemis.fgs_communicator.StoreInIspyb3D.end_deposition")
 @patch("artemis.fgs_communicator.StoreInIspyb3D.begin_deposition")
@@ -244,15 +276,14 @@ def test_writers_do_create_one_file_each_on_start_doc(
 @patch("artemis.fgs_communicator.wait_for_result")
 @patch("artemis.fgs_communicator.run_end")
 @patch("artemis.fgs_communicator.run_start")
-@patch("bluesky.plan_stubs.abs_set")
 def test_communicator_in_composite_run(
-    abs_set: MagicMock,
     run_start: MagicMock,
     run_end: MagicMock,
     wait_for_result: MagicMock,
     nexus_writer: MagicMock,
     ispyb_begin_deposition: MagicMock,
     ispyb_end_deposition: MagicMock,
+    eiger: EigerDetector,
 ):
     nexus_writer.side_effect = [MagicMock(), MagicMock()]
     RE = RunEngine({})
@@ -263,15 +294,23 @@ def test_communicator_in_composite_run(
     communicator = FGSCommunicator(params)
     communicator.xray_centre_motor_position = Point3D(1, 2, 3)
 
-    RE(get_plan(params, communicator))
+    fast_grid_scan_composite = FGSComposite(
+        insertion_prefix=params.insertion_prefix,
+        name="fgs",
+        prefix=params.beamline,
+    )
+    fast_grid_scan_composite.fast_grid_scan.is_invalid = lambda: False
+    fast_grid_scan_composite.wait_for_connection()
+    # Would be better to use get_plan instead but eiger doesn't work well in S03
+    RE(run_gridscan_and_move(fast_grid_scan_composite, eiger, params, communicator))
 
     # nexus writing
-    assert communicator.nxs_writer_1.create_nexus_file.call_count == 1
-    assert communicator.nxs_writer_2.create_nexus_file.call_count == 1
+    communicator.nxs_writer_1.assert_called_once()
+    communicator.nxs_writer_2.assert_called_once()
     # ispyb
     ispyb_begin_deposition.assert_called_once()
     ispyb_end_deposition.assert_called_once()
     # zocalo
     run_start.assert_called()
     run_end.assert_called()
-    wait_for_result.assert_called()
+    wait_for_result.assert_called_once()
