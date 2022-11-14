2058af415384b0387e617b23081fa31cbfca8d13
David Perl <david.perl@diamond.ac.uk>
2022-11-14 10:07:59 +0000

    #348 fix classes so all() works

--- src/artemis/utils.py
+++ src/artemis/utils.py
@@ -1,31 +1,58 @@
 import numpy as np
 
 
-class Point3D(np.ndarray):
-    def __new__(self, x: float, y: float, z: float):
-        a = super().__new__(Point3D, shape=3, dtype=float)
-        a.x = a[0] = x
-        a.y = a[1] = y
-        a.z = a[2] = z
-        return a
-
-    def __array_finalize__(self, a):
-        if a is None:
-            return
-        self.x = self[0]
-        self.y = self[1]
-        self.z = self[2]
-
-
 class Point2D(np.ndarray):
-    def __new__(self, x: float, y: float):
-        a = super().__new__(Point2D, shape=2, dtype=float)
-        a.x = a[0] = x
-        a.y = a[1] = y
-        return a
+    def __new__(cls, x: float, y: float):
+        obj = super().__new__(cls, (2,), float, np.asarray((x, y)))
+        obj.x = x
+        obj.y = y
+        return obj
 
-    def __array_finalize__(self, a):
-        if a is None:
-            return
-        self.x = self[0]
-        self.y = self[1]
+    @property
+    def x(self):
+        return self[0]
+
+    @x.setter
+    def x(self, x):
+        self[0] = x
+
+    @property
+    def y(self):
+        return self[1]
+
+    @y.setter
+    def y(self, y):
+        self[1] = y
+
+
+class Point3D(np.ndarray):
+    def __new__(cls, x: float, y: float, z: float):
+        obj = super().__new__(cls, (3,), float, np.asarray((x, y, z)))
+        obj.x = x
+        obj.y = y
+        obj.z = z
+        return obj
+
+    @property
+    def x(self):
+        return self[0]
+
+    @x.setter
+    def x(self, x):
+        self[0] = x
+
+    @property
+    def y(self):
+        return self[1]
+
+    @y.setter
+    def y(self, y):
+        self[1] = y
+
+    @property
+    def z(self):
+        return self[2]
+
+    @z.setter
+    def z(self, z):
+        self[2] = z
