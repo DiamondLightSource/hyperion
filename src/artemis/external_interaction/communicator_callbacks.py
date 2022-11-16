ffafa8a3d891a90f3e5342ee020590c0f17eb409
David Perl <david.perl@diamond.ac.uk>
2022-11-16 14:33:46 +0000

    #315 add docstring and comment

--- src/artemis/external_interaction/communicator_callbacks.py
+++ src/artemis/external_interaction/communicator_callbacks.py
@@ -140,6 +140,12 @@ class ZocaloHandlerCallback(CallbackBase):
 
 
 class FGSCallbackCollection(NamedTuple):
+    """Groups the callbacks for external interactions in the fast grid scan, and
+    connects the Zocalo and ISPyB handlers. Cast to a list to pass it to
+    Bluesky.preprocessors.subs_decorator()."""
+
+    # Callbacks are triggered in this order, which is important: ISPyB deposition must
+    # be initialised before the Zocalo handler can do its thing.
     nexus_handler: NexusFileHandlerCallback
     ispyb_handler: ISPyBHandlerCallback
     zocalo_handler: ZocaloHandlerCallback
