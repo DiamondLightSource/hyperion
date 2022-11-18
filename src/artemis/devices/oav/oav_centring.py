82e5214c2c62ba657a2d93b68c3be3c776328aa1
Eva Lott <evalott100@gmail.com>
2022-11-18 16:30:45 +0000

    Made setup compatible OAVCentring

--- src/artemis/devices/oav/oav_centring.py
+++ src/artemis/devices/oav/oav_centring.py
@@ -1,8 +1,8 @@
 import json
-from enum import IntEnum
 
 import bluesky.plan_stubs as bps
 import bluesky.preprocessors as bpp
+import numpy as np
 from bluesky import RunEngine
 
 from artemis.devices.backlight import Backlight
@@ -13,21 +13,6 @@ from artemis.devices.oav.oav_detector import (
     ColorMode,
     EdgeOutputArrayImageType,
 )
-
-
-# Not currently in use, but will be when we add later logic
-class Direction(IntEnum):
-    LEFT_TO_RIGHT = 1
-    RIGHT_TO_LEFT = 2
-    TOP_TO_BOTTOM = 3
-
-
-# Not currently in use, but will be when we add later logic
-class NewDirection(IntEnum):
-    RIGHT_TO_LEFT = 0
-    BOTTOM_TO_TOP = 1
-    LEFT_TO_RIGHT = 2
-    TOP_TO_BOTTOM = 3
 
 
 class OAVParameters:
@@ -52,47 +37,39 @@ class OAVParameters:
 
         self.load_json()
 
-        self.exposure = self._extract_dict_parameter(self.context, "exposure")
-        self.acquire_period = self._extract_dict_parameter(self.context, "acqPeriod")
-        self.gain = self._extract_dict_parameter(self.context, "gain")
+        self.exposure = self._extract_dict_parameter("exposure")
+        self.acquire_period = self._extract_dict_parameter("acqPeriod")
+        self.gain = self._extract_dict_parameter("gain")
         self.canny_edge_upper_threshold = self._extract_dict_parameter(
-            self.context, "CannyEdgeUpperThreshold"
+            "CannyEdgeUpperThreshold"
         )
         self.canny_edge_lower_threshold = self._extract_dict_parameter(
-            self.context, "CannyEdgeLowerThreshold", fallback_value=5.0
+            "CannyEdgeLowerThreshold", fallback_value=5.0
         )
-        self.minimum_height = self._extract_dict_parameter(self.context, "minheight")
-        self.zoom = self._extract_dict_parameter(self.context, "zoom")
+        self.minimum_height = self._extract_dict_parameter("minheight")
+        self.zoom = self._extract_dict_parameter("zoom")
         # gets blur type, e.g. 8 = gaussianBlur, 9 = medianBlur
-        self.preprocess = self._extract_dict_parameter(self.context, "preprocess")
+        self.preprocess = self._extract_dict_parameter("preprocess")
         # length scale for blur preprocessing
-        self.preprocess_K_size = self._extract_dict_parameter(
-            self.context, "preProcessKSize"
-        )
-        self.filename = self._extract_dict_parameter(self.context, "filename")
+        self.preprocess_K_size = self._extract_dict_parameter("preProcessKSize")
+        self.filename = self._extract_dict_parameter("filename")
         self.close_ksize = self._extract_dict_parameter(
-            self.context, "close_ksize", fallback_value=11
+            "close_ksize", fallback_value=11
         )
 
-        self.input_plugin = self._extract_dict_parameter(
-            self.context, "oav", fallback_value="OAV"
-        )
+        self.input_plugin = self._extract_dict_parameter("oav", fallback_value="OAV")
         self.mxsc_input = self._extract_dict_parameter(
-            self.context, "mxsc_input", fallback_value="CAM"
+            "mxsc_input", fallback_value="CAM"
         )
         self.min_callback_time = self._extract_dict_parameter(
-            self.context, "min_callback_time", fallback_value=0.08
+            "min_callback_time", fallback_value=0.08
         )
 
-        self.direction = self._extract_dict_parameter(self.context, "direction")
+        self.direction = self._extract_dict_parameter("direction")
 
-        self.max_tip_distance = self._extract_dict_parameter(
-            self.context, "max_tip_distance"
-        )
+        self.max_tip_distance = self._extract_dict_parameter("max_tip_distance")
 
-    def _extract_dict_parameter(
-        self, context: str, key: str, fallback_value=None, reload_json=False
-    ):
+    def _extract_dict_parameter(self, key: str, fallback_value=None, reload_json=False):
         """
         Designed to extract parameters from the json OAVParameters.json. This will hopefully be changed in
         future, but currently we have to use the json passed in from GDA
@@ -104,13 +81,12 @@ class OAVParameters:
                 "context_name": {
                     "parameter1": value3
             }
-        When we extract the parameters we want to check if the given context contains a
-        parameter, if it does we return it, if not we return the global value. If a parameter
-        is not found at all then the passed in fallback_value is returned. If that isn't found
-        then an error is raised.
+        When we extract the parameters we want to check if the given context (stored as a class variable)
+        contains a parameter, if it does we return it, if not we return the global value. If a parameter
+        is not found at all then the passed in fallback_value is returned. If that isn't found then an
+        error is raised.
 
         Args:
-            context: the context to search for the prefered value
             key: the key of the value being extracted
             fallback_value: a value to be returned if the key is not found
             reload_json: reload the json from the file before searching for it, needed because some
@@ -122,9 +98,9 @@ class OAVParameters:
         if reload_json:
             self.load_json()
 
-        if context in self.parameters:
-            if key in self.parameters[context]:
-                return self.parameters[context][key]
+        if self.context in self.parameters:
+            if key in self.parameters[self.context]:
+                return self.parameters[self.context][key]
 
         if key in self.parameters:
             return self.parameters[key]
@@ -134,12 +110,24 @@ class OAVParameters:
 
         # No fallback_value was given and the key wasn't found
         raise KeyError(
-            f"Searched in {self.file} for key {key} in context {context} but no value was found. No fallback value was given."
+            f"Searched in {self.file} for key {key} in context {self.context} but no value was found. No fallback value was given."
         )
+
+    def load_microns_per_pixel(self, zoom):
+        zoom_levels_filename = "src/artemis/oav/microns_for_zoom_levels.json"
+        f = open(zoom_levels_filename)
+        json_dict = json.load(f)
+        self.micronsPerXPixel = json_dict[zoom]["micronsPerXPixel"]
+        self.micronsPerYPixel = json_dict[zoom]["micronsPerYPixel"]
+        if self.micronsPerXPixel is None or self.micronsPerYPixel is None:
+            raise KeyError(
+                f"Could not find the micronsPer[X,Y]Pixel parameters in {zoom_levels_filename}."
+            )
 
 
 class OAVCentring:
-    def __init__(self, parameters_file, beamline="BL03I"):
+    def __init__(self, parameters_file, display_configuration_path, beamline="BL03I"):
+        self.display_configuration_path = display_configuration_path
         self.oav = OAV(name="oav", prefix=beamline + "-DI-OAV-01:")
         self.oav_camera = Camera(name="oav-camera", prefix=beamline + "-EA-OAV-01:")
         self.oav_backlight = Backlight(name="oav-backlight", prefix=beamline)
@@ -258,6 +246,38 @@ class OAVCentring:
             self.oav.output_array_pv, EdgeOutputArrayImageType.ORIGINAL
         )
 
+    def _extract_beam_position(self):
+        """
+
+        Extracts the beam location in pixels `xCentre` `yCentre` extracted
+        from the file display.configuration. The beam location is manually
+        inputted by the beamline operator GDA by clicking where on screen a
+        scintillator ligths up.
+        """
+        with open(self.display_configuration_path, "r") as f:
+            file_lines = f.readlines()
+            for i in range(len(file_lines)):
+                if file_lines[i].startswith(
+                    "zoomLevel = " + str(self.oav_parameters.zoom)
+                ):
+                    crosshair_x_line = file_lines[i + 1]
+                    crosshair_y_line = file_lines[i + 2]
+                    break
+
+            if crosshair_x_line is None or crosshair_y_line is None:
+                pass  # TODO throw error
+
+            self.beam_centre_x = int(crosshair_x_line.split(" = ")[1])
+            self.beam_centre_y = int(crosshair_y_line.split(" = ")[1])
+
+    def get_formatted_edge_waveforms(self):
+        """
+        Get the waveforms from the PVs as numpy arrays.
+        """
+        top = np.array((yield from bps.rd(self.oav.top_pv)))
+        bottom = np.array((yield from bps.rd(self.oav.bottom_pv)))
+        return (top, bottom)
+
 
 @bpp.run_decorator()
 def oav_plan(oav: OAVCentring):
@@ -266,7 +286,9 @@ def oav_plan(oav: OAVCentring):
 
 if __name__ == "__main__":
     oav = OAVCentring(
-        "src/artemis/devices/unit_tests/test_OAVCentring.json", beamline="S03SIM"
+        "src/artemis/devices/unit_tests/test_OAVCentring.json",
+        "src/artemis/devices/unit_tests/test_display.configuration",
+        beamline="S03SIM",
     )
 
     RE = RunEngine()
--- src/artemis/devices/unit_tests/test_oav_centring.py
+++ src/artemis/devices/unit_tests/test_oav_centring.py
@@ -8,7 +8,9 @@ from artemis.devices.oav.oav_centring import OAVParameters
     [("canny_edge_lower_threshold", 5.0), ("close_ksize", 11), ("direction", 1)],
 )
 def test_oav_parameters_load_parameters_from_json(parameter_name, expected_value):
-    parameters = OAVParameters("src/artemis/devices/unit_tests/test_OAVCentring.json")
+    parameters = OAVParameters(
+        "src/artemis/devices/unit_tests/test_OAVCentring.json",
+    )
     parameters.load_parameters_from_json()
 
     assert parameters.__dict__[parameter_name] == expected_value
@@ -18,9 +20,7 @@ def test_oav__extract_dict_parameter_not_found_fallback_value_present():
     parameters = OAVParameters("src/artemis/devices/unit_tests/test_OAVCentring.json")
     parameters.load_json()
     assert (
-        parameters._extract_dict_parameter(
-            "loopCentring", "a_key_not_in_the_json", fallback_value=1
-        )
+        parameters._extract_dict_parameter("a_key_not_in_the_json", fallback_value=1)
         == 1
     )
 
@@ -29,4 +29,4 @@ def test_oav__extract_dict_parameter_not_found_fallback_value_not_present():
     parameters = OAVParameters("src/artemis/devices/unit_tests/test_OAVCentring.json")
     parameters.load_json()
     with pytest.raises(KeyError):
-        parameters._extract_dict_parameter("loopCentring", "a_key_not_in_the_json")
+        parameters._extract_dict_parameter("a_key_not_in_the_json")
