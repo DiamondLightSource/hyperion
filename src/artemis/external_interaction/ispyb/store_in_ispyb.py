cec01c57d8bf9e5de1e179a9946063a053c5e92c
David Perl <david.perl@diamond.ac.uk>
2022-11-30 11:31:30 +0000

    #391 get rid of ispyb conn in read comment func

--- src/artemis/external_interaction/ispyb/store_in_ispyb.py
+++ src/artemis/external_interaction/ispyb/store_in_ispyb.py
@@ -96,21 +96,25 @@ class StoreInIspyb(ABC):
     def _store_scan_data(self):
         pass
 
+    @TRACER.start_span("read_comment_from_ispyb")
     def get_current_datacollection_comment(self, dcid: int) -> str:
-        """Read the 'comments' field from the given datacollection id ISPyB entry.
+        """Read the 'comments' field from the given datacollection id's ISPyB entry.
         Returns an empty string if the comment is not yet initialised.
         """
-        LOGGER.debug("Getting comment from ISPyB")
-        with ispyb.open(self.ISPYB_CONFIG_FILE) as self.conn:
-            with TRACER.start_span("read_comment_from_ispyb"):
-                with self.Session() as session:
-                    query = session.query(DataCollection).filter(
-                        DataCollection.dataCollectionId == dcid
-                    )
-                    current_comment: str = query.first().comments
-        if current_comment is None:
+        try:
+            LOGGER.debug("Getting comment from ISPyB")
+            with self.Session() as session:
+                query = session.query(DataCollection).filter(
+                    DataCollection.dataCollectionId == dcid
+                )
+                current_comment: str = query.first().comments
+            if current_comment is None:
+                current_comment = ""
+            LOGGER.debug(f"Current comment: {current_comment}")
+        except Exception as e:
+            LOGGER.warn("Exception occured when reading comment from ISPyB database:\n")
+            LOGGER.error(e, exc_info=True)
             current_comment = ""
-        LOGGER.debug(f"Current comment: {current_comment}")
         return current_comment
 
     def update_grid_scan_with_end_time_and_status(
