f21622f067fb19781c015635a7ebf393affefc1d
David Perl <david.perl@diamond.ac.uk>
2022-12-09 16:21:08 +0000

    nevermind

--- src/artemis/device_setup_plans/setup_zebra_for_fgs.py
+++ src/artemis/device_setup_plans/setup_zebra_for_fgs.py
@@ -1,4 +1,4 @@
-import bluesky.plan_stubs as bps
+from bluesky.plan_stubs import abs_set
 
 from artemis.devices.zebra import (
     DISCONNECT,
@@ -14,12 +14,12 @@ from artemis.devices.zebra import (
 
 
 def setup_zebra_for_fgs(zebra: Zebra):
-    yield from bps.abs_set(zebra.output.out_pvs[TTL_DETECTOR], IN3_TTL)
-    yield from bps.abs_set(zebra.output.out_pvs[TTL_SHUTTER], IN4_TTL)
-    yield from bps.abs_set(zebra.output.out_pvs[TTL_XSPRESS3], DISCONNECT)
-    yield from bps.abs_set(zebra.output.pulse_1_input, DISCONNECT)
+    yield from abs_set(zebra.output.out_pvs[TTL_DETECTOR], IN3_TTL)
+    yield from abs_set(zebra.output.out_pvs[TTL_SHUTTER], IN4_TTL)
+    yield from abs_set(zebra.output.out_pvs[TTL_XSPRESS3], DISCONNECT)
+    yield from abs_set(zebra.output.pulse_1_input, DISCONNECT)
 
 
 def set_zebra_shutter_to_manual(zebra: Zebra):
-    yield from bps.abs_set(zebra.output.out_pvs[TTL_DETECTOR], PC_PULSE)
-    yield from bps.abs_set(zebra.output.out_pvs[TTL_SHUTTER], OR1)
+    yield from abs_set(zebra.output.out_pvs[TTL_DETECTOR], PC_PULSE)
+    yield from abs_set(zebra.output.out_pvs[TTL_SHUTTER], OR1)
