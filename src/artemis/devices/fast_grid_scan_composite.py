ef915cdf35aba5653e1cb7f106ae41a5ca4f56b6
David Perl <david.perl@diamond.ac.uk>
2022-12-15 16:51:37 +0000

    #446 add some ophyd info logging to FGSComposite

--- src/artemis/devices/fast_grid_scan_composite.py
+++ src/artemis/devices/fast_grid_scan_composite.py
@@ -1,4 +1,5 @@
 from ophyd import Component, Device, FormattedComponent
+from ophyd.log import logger as ophyd_logger
 
 from artemis.devices.fast_grid_scan import FastGridScan
 from artemis.devices.I03Smargon import I03Smargon
@@ -25,3 +26,15 @@ class FGSComposite(Device):
     def __init__(self, insertion_prefix: str, *args, **kwargs):
         self.insertion_prefix = insertion_prefix
         super().__init__(*args, **kwargs)
+
+    def wait_for_connection(self, all_signals=False, timeout=2):
+        ophyd_logger.info(
+            f"FGSComposite waiting for connection, {'not' if all_signals else ''} waiting for all signals, timeout = {timeout}s.",
+        )
+        try:
+            super().wait_for_connection(all_signals, timeout)
+        except TimeoutError as e:
+            ophyd_logger.error("FGSComposite failed to connect.", exc_info=True)
+            raise e
+        else:
+            ophyd_logger.info("FGSComposite connected.", exc_info=True)
