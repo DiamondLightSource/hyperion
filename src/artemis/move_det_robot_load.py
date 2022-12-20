3653b3242b83350851ffbb2668efda239becff16
N Paterson <neil.paterson@diamond.ac.uk>
2022-12-20 13:10:42 +0000

    minor edits

--- src/artemis/move_det_robot_load.py
+++ src/artemis/move_det_robot_load.py
@@ -8,8 +8,10 @@ det.wait_for_connection()
 
 RE = RunEngine({})
 
+"""pre robot load requirements, these may need to be split up and called during preceeding tasks for efficiency"""
 
 def check_det_move_allowed():
+    """Detector motion is prevented by a collision prevention system that disables power to the motors when an object crosses the robot light curtain. Detector motion can also be prevented directly by the robot when a load operation is underway. This checks both and prints an appropriate error if these restrictions are in place."""
     if det.z_disabled.get() == 0 and det.crate_power.get() == 0:
         pass
     else:
@@ -28,6 +30,7 @@ def check_det_move_allowed():
 
 
 def det_move_robot_load():
+    """Check detector motion is allowed and then move to a safe distance for robot load."""
     check_det_move_allowed()
     if det.in_robot_load_safe_position.get() == 1:
         pass
