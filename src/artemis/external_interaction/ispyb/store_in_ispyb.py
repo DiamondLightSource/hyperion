b7d4be482f3e46c77665e5a8ebebee1498cbcbbf
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-02-06 15:45:37 +0000

    Fix device import

--- src/artemis/external_interaction/ispyb/store_in_ispyb.py
+++ src/artemis/external_interaction/ispyb/store_in_ispyb.py
@@ -6,7 +6,7 @@ import ispyb
 import ispyb.sqlalchemy
 from sqlalchemy.connectors import Connector
 
-import artemis.devices.oav.utils as oav_utils
+import artemis.dodal.oav.utils as oav_utils
 from artemis.external_interaction.ispyb.ispyb_dataclass import Orientation
 from artemis.log import LOGGER
 from artemis.parameters import FullParameters
