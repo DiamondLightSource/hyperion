c7303593ef4e734b369d2029d242f79bd128bdf4
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-02-16 15:13:14 +0000

    (#523) Fix beam off script to reset after being killed

--- beam_off_trickery.sh
+++ beam_off_trickery.sh
@@ -1,13 +1,11 @@
 FOOLED_VALUE="100000"
-CORRECT_VALUE="3"
-
 INIT_VALUE=`caget -t BL03I-EA-FDBK-01:THRESHOLDPC_XBPM2`
 
-if [ $INIT_VALUE = $CORRECT_VALUE ]
-then
-    echo "Correct value found, setting to value to fool BPM"
-    caput BL03I-EA-FDBK-01:THRESHOLDPC_XBPM2 $FOOLED_VALUE
-else
-    echo "Fooled value found, setting to correct value"
-    caput BL03I-EA-FDBK-01:THRESHOLDPC_XBPM2 $CORRECT_VALUE    
-fi
+trap "caput BL03I-EA-FDBK-01:THRESHOLDPC_XBPM2 $INIT_VALUE" INT
+
+echo "Correct value found, setting to value to fool BPM"
+caput BL03I-EA-FDBK-01:THRESHOLDPC_XBPM2 $FOOLED_VALUE
+
+echo "Will set back to $INIT_VALUE on termination"
+
+sleep infinity
