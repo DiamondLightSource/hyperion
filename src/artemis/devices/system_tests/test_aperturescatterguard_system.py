f6d871bba5e06891edde2ebc9fc0889a7215cf42
David Perl <david.perl@diamond.ac.uk>
2023-02-16 17:19:30 +0000

    add some movement tests

--- src/artemis/devices/system_tests/test_aperturescatterguard_system.py
+++ src/artemis/devices/system_tests/test_aperturescatterguard_system.py
@@ -24,11 +24,38 @@ def test_aperturescatterguard_setup(ap_sc: ApertureScatterguard):
 
 
 @pytest.mark.s03
-def test_aperurescatterguard_move_in_plan(ap_sc: ApertureScatterguard):
+def test_aperturescatterguard_move_in_plan(ap_sc: ApertureScatterguard):
     RE = RunEngine({})
     ap_sc.wait_for_connection()
+
+    ap_sc.aperture.z.set(ap_sc.aperture_positions.LARGE[2], wait=True)
 
     def move_to_large():
         yield from bps.abs_set(ap_sc, ap_sc.aperture_positions.LARGE)
 
+    def move_to_medium():
+        yield from bps.abs_set(ap_sc, ap_sc.aperture_positions.MEDIUM)
+
+    def move_to_small():
+        yield from bps.abs_set(ap_sc, ap_sc.aperture_positions.SMALL)
+
+    def move_to_robotload():
+        yield from bps.abs_set(ap_sc, ap_sc.aperture_positions.ROBOT_LOAD)
+
     RE(move_to_large())
+    RE(move_to_medium())
+    RE(move_to_small())
+    RE(move_to_robotload())
+
+
+def test_move_fails_when_not_in_good_starting_pos(ap_sc: ApertureScatterguard):
+    RE = RunEngine({})
+    ap_sc.wait_for_connection()
+
+    ap_sc.aperture.z.set(0, wait=True)
+
+    def move_to_large():
+        yield from bps.abs_set(ap_sc, ap_sc.aperture_positions.LARGE)
+
+    with pytest.raises(Exception):
+        RE(move_to_large())
