3cf68f14fc2ab0b0b03376a6aa418439e630ba0f
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-02-21 15:41:13 +0000

    (#469) Fixed minor typo

--- src/artemis/devices/s4_slit_gaps.py
+++ src/artemis/devices/s4_slit_gaps.py
@@ -2,7 +2,7 @@ from ophyd import Component, Device, EpicsMotor
 
 
 class S4SlitGaps(Device):
-    """Note that the S$ slits have a different PV fromat to other beamline slits"""
+    """Note that the S4 slits have a different PV fromat to other beamline slits"""
 
     xgap: EpicsMotor = Component(EpicsMotor, "XGAP")
     ygap: EpicsMotor = Component(EpicsMotor, "YGAP")
