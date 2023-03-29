9c0216a967f820262769cec0afe5a5db6001a544
David Perl <david.perl@diamond.ac.uk>
2023-03-29 11:17:41 +0100

    fix nxs writer test

--- src/artemis/external_interaction/unit_tests/test_write_nexus.py
+++ src/artemis/external_interaction/unit_tests/test_write_nexus.py
@@ -32,7 +32,7 @@ def minimal_params(request):
     params.artemis_params.ispyb_params.flux = 9.0
     params.artemis_params.ispyb_params.transmission = 0.5
     params.artemis_params.detector_params.use_roi_mode = True
-    params.artemis_params.detector_params.num_images_per_trigger = request.param
+    params.artemis_params.detector_params.num_triggers = request.param
     params.artemis_params.detector_params.directory = (
         os.path.dirname(os.path.realpath(__file__)) + "/test_data"
     )
