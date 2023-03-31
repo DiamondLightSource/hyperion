a1f4e0721032a1429a28a60340fb63c97d801436
David Perl <david.perl@diamond.ac.uk>
2023-03-31 16:31:43 +0100

    fix import

--- src/artemis/external_interaction/system_tests/conftest.py
+++ src/artemis/external_interaction/system_tests/conftest.py
@@ -13,7 +13,7 @@ from artemis.external_interaction.ispyb.store_in_ispyb import (
     StoreInIspyb2D,
     StoreInIspyb3D,
 )
-from artemis.parameters.internal_parameters.plan_specific.fgs_internal_parameters import (
+from artemis.parameters.internal_parameters.plan_specific.fgs_internal_params import (
     FGSInternalParameters,
 )
 from artemis.utils import Point3D
