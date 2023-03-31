a8f287c2a8a173cd0a9141ae0a1ca85c6bd6ce06
David Perl <david.perl@diamond.ac.uk>
2023-03-31 15:59:17 +0100

    update param defaults and schema version

--- src/artemis/parameters/external_parameters.py
+++ src/artemis/parameters/external_parameters.py
@@ -28,6 +28,6 @@ def from_json(json_params: str):
     return validate_raw_parameters_from_dict(dict_params)
 
 
-def from_file(json_filename: str = "test_parameters.json"):
+def from_file(json_filename: str = "test_parameter_defaults.json"):
     with open(json_filename) as f:
         return from_json(f.read())
