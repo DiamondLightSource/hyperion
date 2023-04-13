0cadbca701cccda452cc641af53b77ff97ca840a
Ollie Silvester <oliver.silvester@diamond.ac.uk>
2023-04-13 18:30:42 +0100

    Added log which rollsover on time and size

--- src/artemis/log.py
+++ src/artemis/log.py
@@ -1,4 +1,7 @@
 import logging
+import logging.handlers
+import os
+import time
 from os import environ
 from pathlib import Path
 from typing import List, Optional, Tuple, Union
@@ -10,11 +13,127 @@ from ophyd.log import config_ophyd_logging
 from ophyd.log import logger as ophyd_logger
 
 beamline: Union[str, None] = environ.get("BEAMLINE")
-
 LOGGER = logging.getLogger("Artemis")
 LOGGER.setLevel(logging.DEBUG)  # default logger to log everything
 ophyd_logger.parent = LOGGER
 bluesky_logger.parent = LOGGER
+
+
+class EnhancedRotatingFileHandler(logging.handlers.TimedRotatingFileHandler):
+    """Combines features of TimedRotatingFileHandler and RotatingFileHandler"""
+
+    def __init__(
+        self,
+        filename,
+        when="D",
+        interval=1,
+        backupCount=10,
+        encoding=None,
+        delay=0,
+        utc=0,
+        maxBytes=1e9,
+    ):
+        logging.handlers.TimedRotatingFileHandler.__init__(
+            self, filename, when, interval, backupCount, encoding, delay, utc
+        )
+        self.maxBytes = maxBytes
+
+    def shouldRollover(self, record):
+        """
+        Check file size and times to see if rollover should occur
+        """
+        if self.stream is None:  # delay was set...
+            self.stream = self._open()
+        if self.maxBytes > 0:  # are we rolling over?
+            msg = "%s\n" % self.format(record)
+            self.stream.seek(0, 2)  # due to non-posix-compliant Windows feature
+            if self.stream.tell() + len(msg) >= self.maxBytes:
+                return 1
+        t = int(time.time())
+        if t >= self.rolloverAt:
+            return 1
+        return 0
+
+    def doRollover(self):
+        """
+        do a rollover; in this case, a date/time stamp is appended to the filename
+        when the rollover happens.  However, you want the file to be named for the
+        start of the interval, not the current time.  If there is a backup count,
+        then we have to get a list of matching filenames, sort them and remove
+        the one with the oldest suffix.
+        """
+        if self.stream:
+            self.stream.close()
+        # get the time that this sequence started at and make it a TimeTuple
+        currentTime = int(time.time())
+        dstNow = time.localtime(currentTime)[-1]
+        t = self.rolloverAt - self.interval
+        if self.utc:
+            timeTuple = time.gmtime(t)
+        else:
+            timeTuple = time.localtime(t)
+            dstThen = timeTuple[-1]
+            if dstNow != dstThen:
+                if dstNow:
+                    addend = 3600
+                else:
+                    addend = -3600
+                timeTuple = time.localtime(t + addend)
+        dfn = self.baseFilename + "." + time.strftime(self.suffix, timeTuple)
+        if self.backupCount > 0:
+            cnt = 1
+            dfn2 = "%s.%03d" % (dfn, cnt)
+            while os.path.exists(dfn2):
+                dfn2 = "%s.%03d" % (dfn, cnt)
+                cnt += 1
+            os.rename(self.baseFilename, dfn2)
+            for s in self.getFilesToDelete():
+                os.remove(s)
+        else:
+            if os.path.exists(dfn):
+                os.remove(dfn)
+            os.rename(self.baseFilename, dfn)
+        # print "%s -> %s" % (self.baseFilename, dfn)
+        self.mode = "w"
+        self.stream = self._open()
+        newRolloverAt = self.computeRollover(currentTime)
+        while newRolloverAt <= currentTime:
+            newRolloverAt = newRolloverAt + self.interval
+        # If DST changes and midnight or weekly rollover, adjust for this.
+        if (self.when == "MIDNIGHT" or self.when.startswith("W")) and not self.utc:
+            dstAtRollover = time.localtime(newRolloverAt)[-1]
+            if dstNow != dstAtRollover:
+                if (
+                    not dstNow
+                ):  # DST kicks in before next rollover, so we need to deduct an hour
+                    addend = -3600
+                else:  # DST bows out before next rollover, so we need to add an hour
+                    addend = 3600
+                newRolloverAt += addend
+        self.rolloverAt = newRolloverAt
+
+    def getFilesToDelete(self):
+        """
+        Determine the files to delete when rolling over.
+
+        More specific than the earlier method, which just used glob.glob().
+        """
+        dirName, baseName = os.path.split(self.baseFilename)
+        fileNames = os.listdir(dirName)
+        result = []
+        prefix = baseName + "."
+        plen = len(prefix)
+        for fileName in fileNames:
+            if fileName[:plen] == prefix:
+                suffix = fileName[plen:-4]
+                if self.extMatch.match(suffix):
+                    result.append(os.path.join(dirName, fileName))
+        result.sort()
+        if len(result) < self.backupCount:
+            result = []
+        else:
+            result = result[: len(result) - self.backupCount]
+        return result
 
 
 class BeamlineFilter(logging.Filter):
@@ -57,7 +176,13 @@ def set_up_logging_handlers(
     handlers: list[logging.Handler] = [
         GELFTCPHandler(graylog_host, graylog_port),
         logging.StreamHandler(),
-        logging.FileHandler(filename=file_path),
+        EnhancedRotatingFileHandler(
+            filename=file_path,
+            when="S",
+            interval=10,
+            backupCount=10,
+            maxBytes=1e9,
+        ),
     ]
     for handler in handlers:
         handler.setFormatter(formatter)
