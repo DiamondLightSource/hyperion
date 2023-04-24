ce134dca6f93a3d5799b052c5457eb5b605580f2
David Perl <david.perl@diamond.ac.uk>
2023-04-24 11:52:24 +0100

    update docstring

--- src/artemis/external_interaction/callbacks/abstract_plan_callback_collection.py
+++ src/artemis/external_interaction/callbacks/abstract_plan_callback_collection.py
@@ -12,7 +12,10 @@ if TYPE_CHECKING:
 
 class AbstractPlanCallbackCollection(ABC):
     """Base class for a collection of callbacks to attach to a plan. Subclasses should
-    also be dataclasses, or override __iter__"""
+    also be dataclasses, or override __iter__. In general, you should use
+    '@dataclass(frozen=True, order=True)' for your subclass, in which case you can use
+    @subs_decorator(list(callback_collection)) to subscribe them to your plan in order.
+    """
 
     @classmethod
     @abstractmethod
