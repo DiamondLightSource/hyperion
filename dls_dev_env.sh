eedca384d2c8e527d2ba5576c72bdae86e476f00
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-05-11 14:55:19 +0100

    Add blueapi to dependencies

--- dls_dev_env.sh
+++ dls_dev_env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # controls_dev sets pip up to look at a local pypi server, which is incomplete
-module unload controls_dev 
+# module unload controls_dev 
 
-module load python/3.10
+# module load python/3.10
 
 if [ -d "./.venv" ]
 then
@@ -11,20 +11,21 @@ rm -rf .venv
 fi
 mkdir .venv
 
-python -m venv .venv
+python3.10 -m venv .venv
 source .venv/bin/activate
 
 pip install -e .[dev]
 
 # Ensure we use a local version of dodal
-if [ ! -d "../dodal" ]; then
-  git clone git@github.com:DiamondLightSource/dodal.git ../dodal
-fi
+# if [ ! -d "../dodal" ]; then
+#   git clone git@github.com:DiamondLightSource/dodal.git ../dodal
+# fi
 
-pip uninstall -y dodal
+# pip uninstall -y dodal
 pip install -e ../dodal[dev]
 
 # get dlstbx into our env
-ln -s /dls_sw/apps/dials/latest/latest/modules/dlstbx/src/dlstbx/ .venv/lib/python3.10/site-packages/dlstbx
+# ln -s /dls_sw/apps/dials/latest/latest/modules/dlstbx/src/dlstbx/ .venv/lib/python3.10/site-packages/dlstbx
+scp -r ssh.diamond.ac.uk:/dls_sw/apps/dials/latest/latest/modules/dlstbx/src/dlstbx/ .venv/lib/python3.10/site-packages/dlstbx
 
-pytest -m "not s03"
+# pytest -m "not s03"
