5d893e29ebc17a41f8eb6e348e4f90a6c6230811
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-05-22 15:46:55 +0100

    Fixes and cleanup

--- deploy/deploy_artemis.py
+++ deploy/deploy_artemis.py
@@ -21,11 +21,12 @@ class repo:
         print(f"Found {self.name}_versions:\n{os.linesep.join(self.versions)}")
         self.latest_version_str = self.versions[0]
 
-    def deploy(self):
+    def deploy(self, url):
         print(f"Cloning latest version {self.name} into {self.deploy_location}")
 
         deploy_repo = Repo.init(self.deploy_location)
         deploy_origin = deploy_repo.create_remote("origin", self.origin.url)
+
         deploy_origin.fetch()
         deploy_repo.git.checkout(self.latest_version_str)
 
@@ -38,36 +39,6 @@ class repo:
         # Set permissions and defaults
         os.system(f"setfacl -R -m {setfacl_params} {self.deploy_location}")
         os.system(f"setfacl -dR -m {setfacl_params} {self.deploy_location}")
-
-        # Change working directory
-        os.chdir(self.deploy_location)
-        print(f"Setting up environment in {self.deploy_location}")
-
-        if self.name == "artemis":
-            with Popen(
-                "./dls_dev_env.sh", stdout=PIPE, bufsize=1, universal_newlines=True
-            ) as p:
-                if p.stdout is not None:
-                    for line in p.stdout:
-                        print(line, end="")
-
-        if p.returncode != 0:
-            raise CalledProcessError(p.returncode, p.args)
-
-        move_symlink = input(
-            """Move symlink (y/n)? WARNING: this will affect the running version!
-    Only do so if you have informed the beamline scientist and you're sure Artemis is not running.
-    """
-        )
-        if move_symlink == "y":
-            live_location = os.path.join(release_area, self.name)
-            new_tmp_location = os.path.join(release_area, "tmp_art")
-            os.symlink(self.deploy_location, new_tmp_location)
-            os.rename(new_tmp_location, live_location)
-            print(f"New version moved to {live_location}")
-            print("To start this version run artemis_restart from the beamline's GDA")
-        else:
-            print("Quiting without latest version being updated")
 
     # run this after initialising repos, then make function for repo set deply location
     def set_deploy_location(self, release_area):
@@ -102,7 +73,7 @@ def get_artemis_release_dir_from_args(repo: repo) -> str:
         return f"/tmp/artemis_release_test/bluesky/artemis_{repo.latest_version_str}"
     else:
         raise Exception("not running in dev mode, exiting... (remove this)")
-        return f"/dls_sw/{args.beamline}/software/bluesky/artemis_"
+        return f"/dls_sw/{args.beamline}/software/bluesky/artemis_{repo.latest_version_str}"
 
 
 if __name__ == "__main__":
@@ -125,6 +96,48 @@ if __name__ == "__main__":
     dodal_repo.set_deploy_location(release_area)
     artemis_repo.set_deploy_location(release_area)
 
-    # Do deployment
-    artemis_repo.deploy()
-    dodal_repo.deploy()
+    # Deploy artemis repo
+    artemis_repo.deploy(artemis_repo.origin.url)
+
+    # Get version of dodal that latest artemis version uses
+    with open(f"{release_area}/artemis/setup.cfg", "r") as setup_file:
+        # Note if setup.cfg changes, this line will also need to
+        dodal_url = setup_file.readlines()[37]
+
+    dodal_url = dodal_url[dodal_url.find("https") :]
+
+    dodal_repo.deploy(dodal_url)
+
+    # Set up environment and run /dls_dev_env.sh
+
+    # Change working directory
+    os.chdir(artemis_repo.deploy_location)
+    print(f"Setting up environment in {artemis_repo.deploy_location}")
+
+    if artemis_repo.name == "artemis":
+        with Popen(
+            "./dls_dev_env.sh", stdout=PIPE, bufsize=1, universal_newlines=True
+        ) as p:
+            if p.stdout is not None:
+                for line in p.stdout:
+                    print(line, end="")
+
+    if p.returncode != 0:
+        raise CalledProcessError(p.returncode, p.args)
+
+    move_symlink = input(
+        """Move symlink (y/n)? WARNING: this will affect the running version!
+Only do so if you have informed the beamline scientist and you're sure Artemis is not running.
+"""
+    )
+    if move_symlink == "y":
+        live_location = os.path.join(release_area, f"{artemis_repo.name}/artemis")
+        new_tmp_location = os.path.join(release_area, "tmp_art")
+        os.symlink(artemis_repo.deploy_location, new_tmp_location)
+        os.rename(new_tmp_location, live_location)
+        print(f"New version moved to {live_location}")
+        print("To start this version run artemis_restart from the beamline's GDA")
+    else:
+        print("Quiting without latest version being updated")
+
+    # -------------------------------this section is just for artemis--------------------------
