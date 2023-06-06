169795725aae20e550c52756253ce82b22f44318
David Perl <david.perl@diamond.ac.uk>
2023-06-06 08:48:55 +0100

    move ispybparams contents to base class

--- src/artemis/external_interaction/ispyb/ispyb_dataclass.py
+++ src/artemis/external_interaction/ispyb/ispyb_dataclass.py
@@ -33,78 +33,60 @@ GRIDSCAN_ISPYB_PARAM_DEFAULTS = {
 
 class IspybParams(BaseModel):
     visit_path: str
+    microns_per_pixel_x: float
+    microns_per_pixel_y: float
+    position: np.ndarray
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {np.ndarray: lambda a: a.tolist()}
+
+    def dict(self, **kwargs):
+        as_dict = super().dict(**kwargs)
+        as_dict["position"] = as_dict["position"].tolist()
+        return as_dict
+
+    @validator("position", pre=True)
+    def _parse_position(
+        cls, position: list[int | float] | np.ndarray, values: Dict[str, Any]
+    ) -> np.ndarray:
+        assert len(position) == 3
+        if isinstance(position, np.ndarray):
+            return position
+        return np.array(position)
+
+    transmission: float
+    flux: float
+    wavelength: float
+    beam_size_x: float
+    beam_size_y: float
+    focal_spot_size_x: float
+    focal_spot_size_y: float
+    comment: str
+    resolution: float
+
+    sample_id: Optional[int] = None
+    sample_barcode: Optional[str] = None
+
+    # Optional from GDA as populated by Ophyd
+    undulator_gap: Optional[float] = None
+    synchrotron_mode: Optional[str] = None
+    slit_gap_size_x: Optional[float] = None
+    slit_gap_size_y: Optional[float] = None
+    xtal_snapshots_omega_start: Optional[List[str]] = None
+    xtal_snapshots_omega_end: Optional[List[str]] = None
 
 
 class RotationIspybParams(IspybParams):
-    microns_per_pixel_x: float
-    microns_per_pixel_y: float
-    upper_left: np.ndarray
-    position: np.ndarray
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {np.ndarray: lambda a: a.tolist()}
-
-    def dict(self, **kwargs):
-        as_dict = super().dict(**kwargs)
-        as_dict["upper_left"] = as_dict["upper_left"].tolist()
-        as_dict["position"] = as_dict["position"].tolist()
-        return as_dict
-
-    @validator("upper_left", pre=True)
-    def _parse_upper_left(
-        cls, upper_left: list[int | float] | np.ndarray, values: Dict[str, Any]
-    ) -> np.ndarray:
-        assert len(upper_left) == 3
-        if isinstance(upper_left, np.ndarray):
-            return upper_left
-        return np.array(upper_left)
-
-    @validator("position", pre=True)
-    def _parse_position(
-        cls, position: list[int | float] | np.ndarray, values: Dict[str, Any]
-    ) -> np.ndarray:
-        assert len(position) == 3
-        if isinstance(position, np.ndarray):
-            return position
-        return np.array(position)
-
-    transmission: float
-    flux: float
-    wavelength: float
-    beam_size_x: float
-    beam_size_y: float
-    focal_spot_size_x: float
-    focal_spot_size_y: float
-    comment: str
-    resolution: float
-
-    sample_id: Optional[int] = None
-    sample_barcode: Optional[str] = None
-
-    # Optional from GDA as populated by Ophyd
-    undulator_gap: Optional[float] = None
-    synchrotron_mode: Optional[str] = None
-    slit_gap_size_x: Optional[float] = None
-    slit_gap_size_y: Optional[float] = None
-    xtal_snapshots_omega_start: Optional[List[str]] = None
-    xtal_snapshots_omega_end: Optional[List[str]] = None
+    ...
 
 
 class GridscanIspybParams(IspybParams):
-    microns_per_pixel_x: float
-    microns_per_pixel_y: float
     upper_left: np.ndarray
-    position: np.ndarray
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {np.ndarray: lambda a: a.tolist()}
 
     def dict(self, **kwargs):
         as_dict = super().dict(**kwargs)
         as_dict["upper_left"] = as_dict["upper_left"].tolist()
-        as_dict["position"] = as_dict["position"].tolist()
         return as_dict
 
     @validator("upper_left", pre=True)
@@ -115,36 +97,6 @@ class GridscanIspybParams(IspybParams):
         if isinstance(upper_left, np.ndarray):
             return upper_left
         return np.array(upper_left)
-
-    @validator("position", pre=True)
-    def _parse_position(
-        cls, position: list[int | float] | np.ndarray, values: Dict[str, Any]
-    ) -> np.ndarray:
-        assert len(position) == 3
-        if isinstance(position, np.ndarray):
-            return position
-        return np.array(position)
-
-    transmission: float
-    flux: float
-    wavelength: float
-    beam_size_x: float
-    beam_size_y: float
-    focal_spot_size_x: float
-    focal_spot_size_y: float
-    comment: str
-    resolution: float
-
-    sample_id: Optional[int] = None
-    sample_barcode: Optional[str] = None
-
-    # Optional from GDA as populated by Ophyd
-    undulator_gap: Optional[float] = None
-    synchrotron_mode: Optional[str] = None
-    slit_gap_size_x: Optional[float] = None
-    slit_gap_size_y: Optional[float] = None
-    xtal_snapshots_omega_start: Optional[List[str]] = None
-    xtal_snapshots_omega_end: Optional[List[str]] = None
 
 
 class Orientation(Enum):
