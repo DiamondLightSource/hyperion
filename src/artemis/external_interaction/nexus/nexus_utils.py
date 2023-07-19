4a07b76fda260f69554e4c16d13a99d06928144a
David Perl <david.perl@diamond.ac.uk>
2023-07-19 09:44:59 +0100

    fix linting

--- src/artemis/external_interaction/nexus/nexus_utils.py
+++ src/artemis/external_interaction/nexus/nexus_utils.py
@@ -10,7 +10,6 @@ from nexgen.nxs_utils.Axes import TransformationType
 from artemis.external_interaction.ispyb.ispyb_dataclass import IspybParams
 
 
-
 def create_goniometer_axes(
     omega_start: float,
     scan_points: dict | None,
@@ -56,7 +55,9 @@ def create_goniometer_axes(
             start_pos=0.0,
             increment=x_y_z_increments[0],
         ),
-        Axis("chi", "sam_x", TransformationType.ROTATION, (0.006, -0.0264, 0.9996), 0.0),
+        Axis(
+            "chi", "sam_x", TransformationType.ROTATION, (0.006, -0.0264, 0.9996), 0.0
+        ),
         Axis("phi", "chi", TransformationType.ROTATION, (-1, -0.0025, -0.0056), 0.0),
     ]
     return Goniometer(gonio_axes, scan_points)
