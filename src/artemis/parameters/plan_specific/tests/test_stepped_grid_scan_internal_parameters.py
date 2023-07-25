815e0cd6e71a1cb9a4fdf8a0b033d695fb27e2b1
Tom Willemsen <tom.willemsen@diamond.ac.uk>
2023-07-25 13:01:45 +0100

    Placate the third linter

--- src/artemis/parameters/plan_specific/tests/test_stepped_grid_scan_internal_parameters.py
+++ src/artemis/parameters/plan_specific/tests/test_stepped_grid_scan_internal_parameters.py
@@ -1,6 +1,3 @@
-import numpy as np
-from dodal.devices.det_dim_constants import EIGER2_X_16M_SIZE
-
 from artemis.parameters import external_parameters
 from artemis.parameters.plan_specific.stepped_grid_scan_internal_params import (
     SteppedGridScanInternalParameters,
