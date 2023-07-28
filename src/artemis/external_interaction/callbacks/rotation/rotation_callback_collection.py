9b9de39b33da258d184b4e55eeb149d351993498
David Perl <david.perl@diamond.ac.uk>
2023-07-28 17:01:06 +0100

    add rotation zocalo callback

--- src/artemis/external_interaction/callbacks/rotation/rotation_callback_collection.py
+++ src/artemis/external_interaction/callbacks/rotation/rotation_callback_collection.py
@@ -12,9 +12,14 @@ from artemis.external_interaction.callbacks.rotation.ispyb_callback import (
 from artemis.external_interaction.callbacks.rotation.nexus_callback import (
     RotationNexusFileHandlerCallback,
 )
+from artemis.external_interaction.callbacks.rotation.zocalo_callback import (
+    RotationZocaloCallback,
+)
 
 if TYPE_CHECKING:
-    from artemis.parameters.internal_parameters import InternalParameters
+    from artemis.parameters.plan_specific.rotation_scan_internal_params import (
+        RotationInternalParameters,
+    )
 
 
 @dataclass(frozen=True, order=True)
@@ -24,12 +29,18 @@ class RotationCallbackCollection(AbstractPlanCallbackCollection):
 
     nexus_handler: RotationNexusFileHandlerCallback
     ispyb_handler: RotationISPyBHandlerCallback
+    zocalo_handler: RotationZocaloCallback
 
     @classmethod
-    def from_params(cls, parameters: InternalParameters):
+    def from_params(cls, parameters: RotationInternalParameters):
         nexus_handler = RotationNexusFileHandlerCallback()
         ispyb_handler = RotationISPyBHandlerCallback(parameters)
+        zocalo_handler = RotationZocaloCallback(
+            parameters.artemis_params.zocalo_environment, ispyb_handler
+        )
         callback_collection = cls(
-            nexus_handler=nexus_handler, ispyb_handler=ispyb_handler
+            nexus_handler=nexus_handler,
+            ispyb_handler=ispyb_handler,
+            zocalo_handler=zocalo_handler,
         )
         return callback_collection
---
+++ src/artemis/external_interaction/callbacks/rotation/zocalo_callback.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from bluesky.callbacks import CallbackBase
+
+from artemis.external_interaction.callbacks.rotation.ispyb_callback import (
+    RotationISPyBHandlerCallback,
+)
+from artemis.external_interaction.exceptions import ISPyBDepositionNotMade
+from artemis.external_interaction.zocalo.zocalo_interaction import ZocaloInteractor
+from artemis.log import LOGGER
+
+
+class RotationZocaloCallback(CallbackBase):
+    def __init__(
+        self,
+        zocalo_environment: str,
+        ispyb_handler: RotationISPyBHandlerCallback,
+    ):
+        self.ispyb: RotationISPyBHandlerCallback = ispyb_handler
+        self.zocalo_interactor = ZocaloInteractor(zocalo_environment)
+
+    def start(self, doc: dict):
+        LOGGER.info("Zocalo handler received start document.")
+        if doc.get("subplan_name") == "rotation_scan_main":
+            self.run_uid = doc.get("uid")
+            if self.ispyb.ispyb_ids[0] is not None:
+                self.zocalo_interactor.run_start(self.ispyb.ispyb_ids[0])
+            else:
+                raise ISPyBDepositionNotMade("ISPyB deposition was not initialised!")
+
+    def stop(self, doc: dict):
+        if doc.get("run_start") == self.run_uid:
+            LOGGER.info(
+                f"Zocalo handler received stop document, for run {doc.get('run_start')}."
+            )
+            if self.ispyb.ispyb_ids[0]:
+                self.zocalo_interactor.run_end(self.ispyb.ispyb_ids[0])
+            else:
+                raise ISPyBDepositionNotMade("ISPyB deposition was not initialised!")
