a3987a14d5765c9361ae5772aa4d1f4c02be1df9
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-09-18 12:09:02 +0100

    Temporary commit

--- src/hyperion/device_setup_plans/check_topup.py
+++ src/hyperion/device_setup_plans/check_topup.py
@@ -1,7 +1,22 @@
+# import bluesky.plan_stubs as bps
 from dodal.devices.detector import DetectorParams
+from dodal.devices.synchrotron import Synchrotron, SynchrotronMode
 
-# from dodal.devices.synchrotron import Synchrotron
+ALLOWED_MODES = [SynchrotronMode.USER, SynchrotronMode.SPECIAL]
 
 
-def check_topup_plan(params: DetectorParams):
-    pass
+def check_topup_plan(synchrotron: Synchrotron, params: DetectorParams):
+    tot_exposure_time = params.exposure_time * params.full_number_of_images
+    time_to_topup = synchrotron.top_up.start_countdown
+    print(tot_exposure_time * time_to_topup)
+
+    # if mode precludes gating ie. in decay mode, or not in permitted mode
+    # no need to gate
+    # if enough time for collection before topup (ttt > tot_exp_t)
+    # no need to gate
+    # else
+    # delay (bps.sleep) till end of topup
+
+    # no need for the part of delay required because exp_tot always shorter
+    # for mx
+    # pass
