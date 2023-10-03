fff06dd1d1898e1da23e446f57b2c394985172d5
David Perl <david.perl@diamond.ac.uk>
2023-10-03 17:26:01 +0100

    other exclusions

--- src/hyperion/experiment_plans/oav_grid_detection_plan.py
+++ src/hyperion/experiment_plans/oav_grid_detection_plan.py
@@ -17,7 +17,6 @@ from hyperion.device_setup_plans.setup_oav import (
     wait_for_tip_to_be_found,
 )
 from hyperion.log import LOGGER
-from hyperion.parameters.constants import OAV_REFRESH_DELAY
 
 if TYPE_CHECKING:
     from dodal.devices.oav.oav_parameters import OAVParameters
@@ -94,7 +93,7 @@ def grid_detection_main_plan(
         yield from bps.mv(smargon.omega, angle)
         # need to wait for the OAV image to update
         # See #673 for improvements
-        yield from bps.sleep(OAV_REFRESH_DELAY)
+        yield from bps.sleep(0.3)
 
         tip_x_px, tip_y_px = yield from wait_for_tip_to_be_found(oav.mxsc)
 
