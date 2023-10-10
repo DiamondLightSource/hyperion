919b7dc1a213fe04ee19309360a2a63a33b57c17
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-10-10 09:14:49 +0100

    Start on the tests

---
+++ src/hyperion/device_setup_plans/unit_tests/test_topup_plan.py
@@ -0,0 +1,46 @@
+import pytest
+from bluesky.run_engine import RunEngine
+from dodal.beamlines import i03
+
+from hyperion.device_setup_plans.check_topup import check_topup_and_wait_if_necessary
+from hyperion.parameters import external_parameters
+from hyperion.parameters.internal_parameters import HyperionParameters
+
+
+@pytest.fixture
+def synchrotron():
+    return i03.synchrotron(fake_with_ophyd_sim=True)
+
+
+@pytest.fixture
+def fake_parameters():
+    params = external_parameters.from_file(
+        "src/hyperion/parameters/tests/test_data/hyperion_parameters.json"
+    )
+    parameters = HyperionParameters(**params)
+    return parameters.detector_params
+
+
+def test_when_topup_before_end_of_collection_wait(fake_parameters, synchrotron):
+    synchrotron.top_up.start_countdown.sim_put(20.0)
+    synchrotron.top_up.end_countdown.sim_put(60.0)
+
+    RE = RunEngine()
+    RE(
+        check_topup_and_wait_if_necessary(
+            synchrotron=synchrotron,
+            params=fake_parameters,
+        )
+    )
+
+
+def test_no_waiting_if_decay_mode(fake_parameters, synchrotron):
+    synchrotron.top_up.start_countdown.sim_put(-1)
+
+    RE = RunEngine()
+    RE(
+        check_topup_and_wait_if_necessary(
+            synchrotron=synchrotron,
+            params=fake_parameters,
+        )
+    )
