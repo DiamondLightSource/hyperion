e5a10f06ff9654c19309553de7e98dafb39ae126
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-11-03 14:15:48 +0000

    add panda save/load utility functions

--- src/hyperion/experiment_plans/tmptest.py
+++ src/hyperion/experiment_plans/tmptest.py
@@ -1,17 +1,18 @@
 # remove this file
 import asyncio
 
+from bluesky import RunEngine
 from ophyd_async.core import save_to_yaml
 from ophyd_async.panda import PandA
+
+from hyperion.utils.panda_utils import load_panda, save_panda
 
 
 async def test():
     panda = PandA("I03-PANDA")
     await panda.connect()
-    save_to_yaml()
-    # thing = await panda.ttlout[1].val.read()  # or set to ONE
-    # await panda.ttlout[1].val.set("ONE")  # or set to ONE
-    # print("hi")
+    RE = RunEngine()
+    RE(load_panda(panda))
 
 
 asyncio.run(test())
--- src/hyperion/utils/panda_utils.py
+++ src/hyperion/utils/panda_utils.py
@@ -1,7 +1,14 @@
 """Utility functions for the I03 PandA"""
 from typing import Any, Dict, List
 
-from ophyd_async.core import SignalRW, get_signal_values, walk_rw_signals
+from ophyd_async.core import (
+    SignalRW,
+    get_signal_values,
+    load_from_yaml,
+    save_to_yaml,
+    set_signal_values,
+    walk_rw_signals,
+)
 from ophyd_async.panda import PandA
 
 
@@ -16,13 +23,13 @@ async def read_fast_shutter_output(panda: PandA):
     return await panda.ttlout[2].val.read()["value"]
 
 
-def get_panda_phases(panda: PandA):  # type hints?
+def _get_panda_phases(panda: PandA):
     # Panda has two load phases. If the signal name ends in the string "UNITS", it needs to be loaded first so put in first phase
     signalRW_and_value = yield from get_signal_values(walk_rw_signals(panda))
     phase_1 = {}
     phase_2 = {}
     for signal_name in signalRW_and_value.keys():
-        if signal_name[-5:] == "UNITS":
+        if signal_name[-5:] == "units":
             phase_1[signal_name] = signalRW_and_value[signal_name]
         else:
             phase_2[signal_name] = signalRW_and_value[signal_name]
@@ -30,4 +37,13 @@ def get_panda_phases(panda: PandA):  # type hints?
     return [phase_1, phase_2]
 
 
-# Used to save
+def save_panda(panda: PandA):
+    phases = yield from _get_panda_phases(panda)
+    save_to_yaml(phases, "/scratch/qqh35939/panda_yaml_saves/test.yaml")
+
+
+def load_panda(panda: PandA):
+    phases = yield from _get_panda_phases(panda)
+    values = load_from_yaml("/scratch/qqh35939/panda_yaml_saves/test.yaml")
+    signals_to_set = walk_rw_signals(panda)
+    yield from set_signal_values(signals_to_set, values)
