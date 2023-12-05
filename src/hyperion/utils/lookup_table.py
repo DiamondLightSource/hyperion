2a45274962e03c28e4d9de88032720de01874f34
Robert Tuck <robert.tuck@diamond.ac.uk>
2023-12-05 11:41:08 +0000

    (#986) Remove commented out code

--- src/hyperion/utils/lookup_table.py
+++ src/hyperion/utils/lookup_table.py
@@ -46,17 +46,3 @@ class PerpRollLUTConverter(LookupTableConverter):
     # TODO if we need to read the XML JEPQuantityConverter file instead of hardcoding this
     def s_to_t(self, s):
         return -1.0 * (25 / (2.0 * cos(s * 3.1416 / 180.0)) - 13.56)
-
-
-# class DiscontinuousLinearInterpolation(LookupTableConverter):
-#     """Linearly interpolate between pairs of (s, t) points in the lookup table. Where the LUT provides two points for
-#      the same s-coordinate, the first point provides the control point to be paired with preceding point and the second
-#      with the succeeding point."""
-#     def __init__(self, filename: str):
-#         super().__init__()
-#         s_and_t_vals = zip(*loadtxt(filename, comments=["#", "Units"]))
-#         self._s_values, self._t_values = s_and_t_vals
-#
-#     def s_to_t(self, s):
-#
-#         return interp(s, self._s_values, self._t_values)
