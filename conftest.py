adf1704f8af1fe685f58070e1456af7f548c1316
David Perl <david.perl@diamond.ac.uk>
2023-12-06 13:03:32 +0000

    #947 patch Publisher in main for all tests

--- conftest.py
+++ conftest.py
@@ -1,3 +1,9 @@
+from typing import Iterator
+from unittest.mock import patch
+
+import pytest
+
+
 def pytest_addoption(parser):
     parser.addoption(
         "--debug-logging",
@@ -5,3 +11,10 @@ def pytest_addoption(parser):
         default=False,
         help="initialise test loggers in DEBUG instead of INFO",
     )
+
+
+@pytest.fixture(scope="session", autouse=True)
+def default_session_fixture() -> Iterator[None]:
+    print("Patching bluesky 0MQ Publisher in __main__ for the whole session")
+    with patch("hyperion.__main__.Publisher"):
+        yield
