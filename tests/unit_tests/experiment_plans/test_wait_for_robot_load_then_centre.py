235ad765c5805fb6a2b1f1176336b65c9e877aea
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-12-11 01:18:55 +0000

    (#984) Wait for smargon to be enabled

--- tests/unit_tests/experiment_plans/test_wait_for_robot_load_then_centre.py
+++ tests/unit_tests/experiment_plans/test_wait_for_robot_load_then_centre.py
@@ -2,6 +2,8 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 from bluesky.run_engine import RunEngine
+from dodal.devices.smargon import Smargon
+from ophyd.sim import instantiate_fake_device
 
 from hyperion.experiment_plans.wait_for_robot_load_then_centre import (
     wait_for_robot_load_then_centre,
@@ -13,6 +15,8 @@ from hyperion.parameters.plan_specific.pin_centre_then_xray_centre_params import
 from hyperion.parameters.plan_specific.wait_for_robot_load_then_center_params import (
     WaitForRobotLoadThenCentreInternalParameters,
 )
+
+from .conftest import RunEngineSimulator
 
 
 @pytest.fixture
@@ -31,6 +35,8 @@ def test_when_plan_run_then_centring_plan_run_with_expected_parameters(
     wait_for_robot_load_then_centre_params: WaitForRobotLoadThenCentreInternalParameters,
 ):
     mock_composite = MagicMock()
+    mock_composite.smargon = instantiate_fake_device(Smargon, name="smargon")
+
     RE = RunEngine()
     RE(
         wait_for_robot_load_then_centre(
@@ -44,3 +50,66 @@ def test_when_plan_run_then_centring_plan_run_with_expected_parameters(
 
     assert composite_passed == mock_composite
     assert isinstance(params_passed, PinCentreThenXrayCentreInternalParameters)
+
+
+def run_simulating_smargon_wait(
+    wait_for_robot_load_then_centre_params, total_disabled_reads
+):
+    mock_composite = MagicMock()
+    mock_composite.smargon = instantiate_fake_device(Smargon, name="smargon")
+
+    num_of_reads = 0
+
+    def return_not_disabled_after_reads(_):
+        nonlocal num_of_reads
+        num_of_reads += 1
+        return {"values": {"value": int(num_of_reads < total_disabled_reads)}}
+
+    sim = RunEngineSimulator()
+    sim.add_handler(
+        "read",
+        "smargon_disabled",
+        return_not_disabled_after_reads,
+    )
+
+    return sim.simulate_plan(
+        wait_for_robot_load_then_centre(
+            mock_composite, wait_for_robot_load_then_centre_params
+        )
+    )
+
+
+@pytest.mark.parametrize("total_disabled_reads", [5, 3, 14])
+@patch(
+    "hyperion.experiment_plans.wait_for_robot_load_then_centre.pin_tip_centre_then_xray_centre"
+)
+def test_given_smargon_disabled_when_plan_run_then_waits_on_smargon(
+    mock_centring_plan: MagicMock,
+    wait_for_robot_load_then_centre_params: WaitForRobotLoadThenCentreInternalParameters,
+    total_disabled_reads: int,
+):
+    messages = run_simulating_smargon_wait(
+        wait_for_robot_load_then_centre_params, total_disabled_reads
+    )
+
+    mock_centring_plan.assert_called_once()
+
+    sleep_messages = filter(lambda msg: msg.command == "sleep", messages)
+    read_disabled_messages = filter(
+        lambda msg: msg.command == "read" and msg.obj.name == "smargon_disabled",
+        messages,
+    )
+
+    assert len(list(sleep_messages)) == total_disabled_reads - 1
+    assert len(list(read_disabled_messages)) == total_disabled_reads
+
+
+@patch(
+    "hyperion.experiment_plans.wait_for_robot_load_then_centre.pin_tip_centre_then_xray_centre"
+)
+def test_given_smargon_disabled_for_longer_than_timeout_when_plan_run_then_throws_exception(
+    mock_centring_plan: MagicMock,
+    wait_for_robot_load_then_centre_params: WaitForRobotLoadThenCentreInternalParameters,
+):
+    with pytest.raises(TimeoutError):
+        run_simulating_smargon_wait(wait_for_robot_load_then_centre_params, 1000)
