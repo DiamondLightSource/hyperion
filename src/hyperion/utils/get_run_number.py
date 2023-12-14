6b34d0fdcb7d7e434db0f40527c58e4fe4a6625e
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-12-14 18:36:14 +0000

    create get_run_number_function and tests

---
+++ src/hyperion/utils/get_run_number.py
@@ -0,0 +1,37 @@
+import os
+import re
+from typing import List
+
+import hyperion.log
+
+
+class RunNumberException(Exception):
+    pass
+
+
+def _find_next_run_number_from_files(file_names: List[str]) -> int:
+    valid_numbers = []
+
+    for file_name in file_names:
+        file_name = file_name.strip(".nxs")
+        # Give warning if nexus file name isn't in expcted format, xxx_number.nxs
+        match = re.search(r"_\d+$", file_name)
+        if match is not None:
+            valid_numbers.append(int(re.findall(r"\d+", file_name)[-1]))
+        else:
+            hyperion.log.LOGGER.warning(
+                f"Identified nexus file {file_name} with unexpected format"
+            )
+    try:
+        return max(valid_numbers) + 1
+    except Exception:
+        raise RunNumberException(f"Unable to find the run number")
+
+
+def get_run_number(directory: str) -> int:
+    nexus_file_names = [file for file in os.listdir(directory) if file.endswith(".nxs")]
+
+    if len(nexus_file_names) == 0:
+        return 1
+    else:
+        return _find_next_run_number_from_files(nexus_file_names)
---
+++ tests/unit_tests/utils/test_get_run_number.py
@@ -0,0 +1,65 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from hyperion.utils.get_run_number import (
+    RunNumberException,
+    _find_next_run_number_from_files,
+    get_run_number,
+)
+
+
+def test_find_next_run_number_from_files_gets_correct_number():
+    assert (
+        _find_next_run_number_from_files(
+            ["V31-1-x0093_1.nxs", "V31-1-x0093_2.nxs", "V31-1-x0093_265.nxs"]
+        )
+        == 266
+    )
+
+
+@patch("hyperion.log.LOGGER.warning")
+def test_find_next_run_number_gives_warning_with_wrong_nexus_names(
+    mock_logger: MagicMock,
+):
+    assert (
+        _find_next_run_number_from_files(
+            ["V31-1-x0093.nxs", "eggs", "V31-1-x0093_1.nxs"]
+        )
+        == 2
+    )
+    assert mock_logger.call_count == 2
+
+
+@patch("os.listdir")
+@patch("hyperion.utils.get_run_number._find_next_run_number_from_files")
+def test_get_run_number_finds_all_nexus_files(
+    mock_find_next_run_number: MagicMock, mock_list_dir: MagicMock
+):
+    files = ["blah.nxs", "foo", "bar.nxs", "ham.h5"]
+    mock_list_dir.return_value = files
+    get_run_number("dir")
+    mock_find_next_run_number.assert_called_once_with(["blah.nxs", "bar.nxs"])
+
+
+@patch("os.listdir")
+@patch("hyperion.utils.get_run_number._find_next_run_number_from_files")
+def test_exception_logged_on_invalid_values(
+    mock_find_next_run_number: MagicMock,
+    mock_list_dir: MagicMock,
+):
+    mock_find_next_run_number.return_value = Exception
+    mock_list_dir.return_value = "file.nxs"
+    with pytest.raises(RunNumberException):
+        get_run_number("dir")
+
+
+@patch("os.listdir")
+@patch("hyperion.utils.get_run_number._find_next_run_number_from_files")
+def test_run_number_1_given_on_first_nexus_file(
+    mock_find_next_run_number: MagicMock, mock_list_dir: MagicMock
+):
+    files = ["blah", "foo", "bar"]
+    mock_list_dir.return_value = files
+    assert get_run_number("dir") == 1
+    mock_find_next_run_number.assert_not_called()
