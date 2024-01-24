15e524d621e215b90d40e48b7fb77fffa3bf4cff
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-01-24 10:54:58 +0000

    all tests passing

--- tests/unit_tests/experiment_plans/test_flyscan_xray_centre_plan.py
+++ tests/unit_tests/experiment_plans/test_flyscan_xray_centre_plan.py
@@ -1,6 +1,5 @@
 import random
 import types
-from typing import Callable
 from unittest.mock import MagicMock, call, patch
 
 import bluesky.preprocessors as bpp
@@ -127,7 +126,8 @@ class TestFlyscanXrayCentrePlan:
         fake_fgs_composite: FlyScanXRayCentreComposite,
         test_fgs_params: GridscanInternalParameters,
         RE: RunEngine,
-        ispyb_plan: Callable[[GridscanInternalParameters], None],
+        # ispyb_plan: Callable[[GridscanInternalParameters], None],
+        ispyb_plan,
     ):
         undulator_test_value = 1.234
 
@@ -163,6 +163,7 @@ class TestFlyscanXrayCentrePlan:
                 fake_fgs_composite.s4_slit_gaps,
                 fake_fgs_composite.attenuator,
                 fake_fgs_composite.flux,
+                fake_fgs_composite.aperture_scatterguard,
             )
         )
         params = test_ispyb_callback.params
