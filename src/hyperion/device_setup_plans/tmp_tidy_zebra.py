ee867c02eb2ef33e5c3e937162c36ba7033ea179
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-02-13 17:08:58 +0000

    Panda no longer handles fast shutter control

--- src/hyperion/device_setup_plans/tmp_tidy_zebra.py
+++
@@ -1,9 +0,0 @@
-from bluesky.run_engine import RunEngine
-from dodal.beamlines.i03 import zebra
-
-from hyperion.device_setup_plans.setup_zebra import set_zebra_shutter_to_manual
-
-if __name__ == "__main__":
-    RE = RunEngine()
-    zebra_device = zebra()
-    RE(set_zebra_shutter_to_manual(zebra_device, wait=True))
