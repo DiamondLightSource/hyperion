c210d56d574488b3de54a105eb609d404f9b6fd3
David Perl <david.perl@diamond.ac.uk>
2024-03-04 13:06:54 +0000

    #1068 update hyperion plans to use ophyd-async edge detect

--- src/hyperion/experiment_plans/oav_grid_detection_plan.py
+++ src/hyperion/experiment_plans/oav_grid_detection_plan.py
@@ -8,7 +8,6 @@ import bluesky.plan_stubs as bps
 import bluesky.preprocessors as bpp
 import numpy as np
 from blueapi.core import BlueskyContext
-from bluesky.preprocessors import finalize_wrapper
 from dodal.devices.backlight import Backlight
 from dodal.devices.oav.oav_detector import OAV
 from dodal.devices.oav.pin_image_recognition import PinTipDetection
@@ -17,8 +16,8 @@ from dodal.devices.smargon import Smargon
 from hyperion.device_setup_plans.setup_oav import (
     get_move_required_so_that_beam_is_at_pixel,
     pre_centring_setup_oav,
-    wait_for_tip_to_be_found,
 )
+from hyperion.experiment_plans.pin_tip_centring_plan import trigger_and_return_pin_tip
 from hyperion.log import LOGGER
 from hyperion.parameters.constants import (
     OAV_REFRESH_DELAY,
@@ -43,29 +42,8 @@ def create_devices(context: BlueskyContext) -> OavGridDetectionComposite:
     return device_composite_from_context(context, OavGridDetectionComposite)
 
 
-def grid_detection_plan(
-    composite: OavGridDetectionComposite,
-    parameters: OAVParameters,
-    snapshot_template: str,
-    snapshot_dir: str,
-    grid_width_microns: float,
-    box_size_microns=20,
-):
-    yield from finalize_wrapper(
-        grid_detection_main_plan(
-            composite,
-            parameters,
-            snapshot_template,
-            snapshot_dir,
-            grid_width_microns,
-            box_size_microns,
-        ),
-        reset_oav(composite.oav),
-    )
-
-
 @bpp.run_decorator()
-def grid_detection_main_plan(
+def grid_detection_plan(
     composite: OavGridDetectionComposite,
     parameters: OAVParameters,
     snapshot_template: str,
@@ -87,13 +65,14 @@ def grid_detection_main_plan(
     """
     oav: OAV = composite.oav
     smargon: Smargon = composite.smargon
+    pin_tip_detection: PinTipDetection = composite.pin_tip_detection
 
     LOGGER.info("OAV Centring: Starting grid detection centring")
 
     yield from bps.wait()
 
     # Set relevant PVs to whatever the config dictates.
-    yield from pre_centring_setup_oav(oav, parameters, oav.mxsc)
+    yield from pre_centring_setup_oav(oav, parameters, pin_tip_detection)
 
     LOGGER.info("OAV Centring: Camera set up")
 
@@ -113,13 +92,14 @@ def grid_detection_main_plan(
         # need to wait for the OAV image to update
         # See #673 for improvements
         yield from bps.sleep(OAV_REFRESH_DELAY)
-
-        tip_x_px, tip_y_px = yield from wait_for_tip_to_be_found(oav.mxsc.pin_tip)
+        tip_x_px, tip_y_px = yield from trigger_and_return_pin_tip(pin_tip_detection)
 
         LOGGER.info(f"Tip is at x,y: {tip_x_px},{tip_y_px}")
 
-        top_edge = np.array((yield from bps.rd(oav.mxsc.top)))
-        bottom_edge = np.array((yield from bps.rd(oav.mxsc.bottom)))
+        top_edge = np.array((yield from bps.rd(pin_tip_detection.triggered_top_edge)))
+        bottom_edge = np.array(
+            (yield from bps.rd(pin_tip_detection.triggered_bottom_edge))
+        )
 
         full_image_height_px = yield from bps.rd(oav.cam.array_size.array_size_y)
 
@@ -198,9 +178,3 @@ def grid_detection_main_plan(
     )
 
     LOGGER.info(f"Step sizes: {box_size_um, box_size_um, box_size_um}")
-
-
-def reset_oav(oav: OAV):
-    """Changes the MJPG stream to look at the camera without the edge detection and turns off the edge detcetion plugin."""
-    yield from bps.abs_set(oav.snapshot.input_plugin, "OAV.CAM")
-    yield from bps.abs_set(oav.mxsc.enable_callbacks, 0)
