3876c81891d8c8e21b3abb55db21126c939cf23a
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-03-19 17:06:10 +0000

    Fix some system tests

--- tests/system_tests/hyperion/test_device_setups_and_cleanups.py
+++ tests/system_tests/hyperion/test_device_setups_and_cleanups.py
@@ -1,4 +1,5 @@
 import pytest
+from bluesky.run_engine import RunEngine
 from dodal.devices.zebra import (
     IN3_TTL,
     IN4_TTL,
@@ -18,28 +19,32 @@ from hyperion.device_setup_plans.setup_zebra import (
 
 
 @pytest.fixture
-def connected_zebra():
+async def connected_zebra():
+    RunEngine()
     zebra = Zebra(name="zebra", prefix="BL03S-EA-ZEBRA-01:")
-    zebra.wait_for_connection()
+    await zebra.connect()
     return zebra
 
 
 @pytest.mark.s03
-def test_zebra_set_up_for_gridscan(RE, connected_zebra: Zebra):
+async def test_zebra_set_up_for_gridscan(RE, connected_zebra: Zebra):
     RE(setup_zebra_for_gridscan(connected_zebra, wait=True))
-    assert connected_zebra.output.out_pvs[TTL_DETECTOR].get() == IN3_TTL
-    assert connected_zebra.output.out_pvs[TTL_SHUTTER].get() == IN4_TTL
+    assert await connected_zebra.output.out_pvs[TTL_DETECTOR].get_value() == IN3_TTL
+    assert await connected_zebra.output.out_pvs[TTL_SHUTTER].get_value() == IN4_TTL
 
 
 @pytest.mark.s03
-def test_zebra_set_up_for_rotation(RE, connected_zebra: Zebra):
+async def test_zebra_set_up_for_rotation(RE, connected_zebra: Zebra):
     RE(setup_zebra_for_rotation(connected_zebra, wait=True))
-    assert connected_zebra.pc.gate_trigger.get(as_string=True) == I03Axes.OMEGA.value
-    assert connected_zebra.pc.gate_width.get() == pytest.approx(360, 0.01)
+    assert (
+        await connected_zebra.pc.gate_trigger.get_value(as_string=True)
+        == I03Axes.OMEGA.value
+    )
+    assert await connected_zebra.pc.gate_width.get_value() == pytest.approx(360, 0.01)
 
 
 @pytest.mark.s03
-def test_zebra_cleanup(RE, connected_zebra: Zebra):
+async def test_zebra_cleanup(RE, connected_zebra: Zebra):
     RE(set_zebra_shutter_to_manual(connected_zebra, wait=True))
-    assert connected_zebra.output.out_pvs[TTL_DETECTOR].get() == PC_PULSE
-    assert connected_zebra.output.out_pvs[TTL_SHUTTER].get() == OR1
+    assert await connected_zebra.output.out_pvs[TTL_DETECTOR].get_value() == PC_PULSE
+    assert await connected_zebra.output.out_pvs[TTL_SHUTTER].get_value() == OR1
