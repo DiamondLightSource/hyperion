1448c88d0d822134b330bee225d8a5e7a11236ff
David Perl <david.perl@diamond.ac.uk>
2024-03-27 13:23:56 +0000

    (#698) add test that all plans have correct param input type

--- tests/unit_tests/experiment_plans/test_experiment_registry.py
+++ tests/unit_tests/experiment_plans/test_experiment_registry.py
@@ -1,5 +1,9 @@
+from inspect import getfullargspec
+
 from dodal.parameters.experiment_parameter_base import AbstractExperimentParameterBase
 
+import hyperion.experiment_plans as plan_module
+from hyperion.experiment_plans import __all__ as exposed_plans
 from hyperion.experiment_plans.experiment_registry import PLAN_REGISTRY, do_nothing
 from hyperion.parameters.components import HyperionParameters
 
@@ -13,6 +17,19 @@ def test_experiment_registry_param_types():
         assert issubclass(
             PLAN_REGISTRY[plan]["internal_param_type"], HyperionParameters
         )
+
+
+def test_exposed_plans_in_reg():
+    for plan in exposed_plans:
+        assert plan in PLAN_REGISTRY.keys()
+
+
+def test_param_types_in_registry_match_plan():
+    for plan in exposed_plans:
+        plan_function = getattr(plan_module, plan)
+        plan_args = getfullargspec(plan_function)
+        param_arg_type = plan_args.annotations["parameters"]
+        assert PLAN_REGISTRY[plan]["internal_param_type"].__name__ in param_arg_type
 
 
 def test_do_nothing():
