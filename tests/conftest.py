96ef6228e499d5711af1c3281457f0fe1dcc6a90
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-04-03 12:06:13 +0100

    (#1252) remove teardown check for logging option as per PR review comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -132,7 +132,7 @@ def pytest_runtest_teardown(item):
     if "dodal.beamlines.beamline_utils" in sys.modules:
         sys.modules["dodal.beamlines.beamline_utils"].clear_devices()
     markers = [m.name for m in item.own_markers]
-    if item.config.getoption("logging") and "skip_log_setup" in markers:
+    if "skip_log_setup" in markers:
         _reset_loggers([*ALL_LOGGERS, dodal_logger])
 
 
