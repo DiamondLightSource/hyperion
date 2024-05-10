611a7b1e5382aad1347de9c540c8483e3619e427
David Perl <david.perl@diamond.ac.uk>
2024-05-10 08:55:55 +0100

    (#487) Improve logging formatting for documents

--- src/hyperion/external_interaction/callbacks/logging_callback.py
+++ src/hyperion/external_interaction/callbacks/logging_callback.py
@@ -1,18 +1,29 @@
+import json
+
 from bluesky.callbacks import CallbackBase
 
 from hyperion.log import LOGGER
 
 
+class _BestEffortEncoder(json.JSONEncoder):
+    def default(self, o):
+        return repr(o)
+
+
+def _format(doc):
+    return json.dumps(doc, indent=2, cls=_BestEffortEncoder)
+
+
 class VerbosePlanExecutionLoggingCallback(CallbackBase):
     def start(self, doc):
-        LOGGER.info(f"START: {doc}")
+        LOGGER.info(f"START: {_format(doc)}")
 
     def descriptor(self, doc):
-        LOGGER.info(f"DESCRIPTOR: {doc}")
+        LOGGER.info(f"DESCRIPTOR: {_format(doc)}")
 
     def event(self, doc):
-        LOGGER.info(f"EVENT: {doc}")
+        LOGGER.info(f"EVENT: {_format(doc)}")
         return doc
 
     def stop(self, doc):
-        LOGGER.info(f"STOP: {doc}")
+        LOGGER.info(f"STOP: {_format(doc)}")
