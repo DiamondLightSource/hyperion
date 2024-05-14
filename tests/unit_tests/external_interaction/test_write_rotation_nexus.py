712a22c62842428b354d8780719dffd0e649ec0e
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-05-14 10:15:38 +0100

    (DiamondLightSource/nexgen#231) add more explicit tests for comparison of rotation nexus file

--- tests/unit_tests/external_interaction/test_write_rotation_nexus.py
+++ tests/unit_tests/external_interaction/test_write_rotation_nexus.py
@@ -1,5 +1,3 @@
-"""PYTEST_DONT_REWRITE"""
-
 import os
 from pathlib import Path
 from shutil import copy
@@ -20,6 +18,7 @@ from hyperion.experiment_plans.rotation_scan_plan import RotationScanComposite
 from hyperion.external_interaction.callbacks.rotation.nexus_callback import (
     RotationNexusFileCallback,
 )
+from hyperion.log import LOGGER
 from hyperion.parameters.constants import CONST
 from hyperion.parameters.plan_specific.rotation_scan_internal_params import (
     RotationInternalParameters,
@@ -76,6 +75,40 @@ def fake_rotation_scan(
     return plan()
 
 
+def dectris_device_mapping(meta_filename: str):
+    return {
+        "entry": {
+            "instrument": {
+                "detector": {
+                    "bit_depth_image": f"{meta_filename}//_dectris/bit_depth_image",
+                    "bit_depth_readout": f"{meta_filename}//_dectris/bit_depth_image",
+                    "detectorSpecific": {
+                        "ntrigger": f"{meta_filename}///_dectris/ntrigger",
+                        "software_version": f"{meta_filename}//_dectris/software_version",
+                    },
+                    "detector_readout_time": f"{meta_filename}//_dectris/detector_readout_time",
+                    "flatfield_applied": f"{meta_filename}//_dectris/flatfield_correction_applied",
+                    "photon_energy": f"{meta_filename}//_dectris/photon_energy",
+                    "pixel_mask": f"{meta_filename}//mask",
+                    "pixel_mask_applied": f"{meta_filename}//_dectris/pixel_mask_applied",
+                    "threshold_energy": f"{meta_filename}//_dectris/threshold_energy",
+                }
+            }
+        }
+    }
+
+
+def apply_metafile_mapping(exceptions: dict, mapping: dict):
+    """Recursively populate the exceptions map with corresponding mapping entries"""
+    for key in mapping.keys():
+        mapping_value = mapping.get(key)
+        if isinstance(mapping_value, dict):
+            exceptions_child = exceptions.setdefault(key, {})
+            apply_metafile_mapping(exceptions_child, mapping_value)
+        else:
+            exceptions[key] = mapping_value
+
+
 def test_rotation_scan_nexus_output_compared_to_existing_full_compare(
     test_params: RotationInternalParameters,
     tmpdir,
@@ -84,6 +117,7 @@ def test_rotation_scan_nexus_output_compared_to_existing_full_compare(
     run_number = test_params.hyperion_params.detector_params.run_number
     nexus_filename = f"{tmpdir}/{TEST_FILENAME}_{run_number}.nxs"
     master_filename = f"{tmpdir}/{TEST_FILENAME}_{run_number}_master.h5"
+    meta_filename = f"{TEST_FILENAME}_{run_number}_meta.h5"
 
     fake_create_rotation_devices.eiger.bit_depth.sim_put(32)  # type: ignore
 
@@ -110,65 +144,80 @@ def test_rotation_scan_nexus_output_compared_to_existing_full_compare(
     )
     example_nexus_path = f"{tmpdir}/{TEST_EXAMPLE_NEXUS_FILE}"
     copy(TEST_DATA_DIRECTORY / TEST_EXAMPLE_NEXUS_FILE, example_nexus_path)
+
+    # Models expected differences to the GDA master nexus file
+    # If a key is in _missing then it is not expected to be present
+    # If a key is in _ignore then we do not compare it
+    # If a key maps to a dict then we expect it to be a Group
+    # Otherwise if a key is present we expect it to be a DataSet
+    # If a key maps to a callable then we use that as the comparison function
+    # Otherwise we compare the scalar or array value as appropriate
+    exceptions = {
+        "entry": {
+            "_missing": {"end_time"},
+            "data": {"_ignore": {"data", "omega"}},
+            "instrument": {
+                "_missing": {"transformations", "detector_z", "source"},
+                "detector": {
+                    "_missing": {
+                        "detector_distance",
+                        "serial_number",  # nexgen#236
+                    },
+                    "distance": 0.1,
+                    "transformations": {"detector_z": {"det_z": np.array([100])}},
+                    "detector_z": {"det_z": np.array([100])},
+                    "underload_value": 0,
+                    "_ignore": {
+                        "beam_center_x",
+                        "beam_center_y",
+                        "depends_on",
+                        "saturation_value",
+                        "sensor_material",
+                    },
+                    "detectorSpecific": {
+                        "_missing": {"pixel_mask"},
+                        "x_pixels": 4148,
+                        "y_pixels": 4362,
+                    },
+                    "module": {"_ignore": {"module_offset"}},
+                    "sensor_thickness": np.isclose,
+                },
+                "attenuator": {"attenuator_transmission": np.isclose},
+                "beam": {"incident_wavelength": np.isclose},
+                "name": b"DIAMOND BEAMLINE S03",
+            },
+            "sample": {
+                "beam": {"incident_wavelength": np.isclose},
+                "transformations": {
+                    "_missing": {"omega_end", "omega_increment_set"},
+                    "_ignore": {"omega"},
+                },
+                "sample_omega": {
+                    "_ignore": {"omega_end", "omega"},
+                    "omega_increment_set": 0.1,
+                },
+                "sample_x": {"sam_x": np.isclose},
+                "sample_y": {"sam_y": np.isclose},
+                "sample_z": {"sam_z": np.isclose},
+                "sample_chi": {"chi": np.isclose},
+                "sample_phi": {"phi": np.isclose},
+            },
+            "end_time_estimated": b"test_timeZ",
+            "start_time": b"test_timeZ",
+            "source": {
+                "name": b"Diamond Light Source",
+                "type": b"Synchrotron X-ray Source",
+            },
+        }
+    }
+
     with (
         h5py.File(example_nexus_path, "r") as example_nexus,
         h5py.File(nexus_filename, "r") as hyperion_nexus,
     ):
+        apply_metafile_mapping(exceptions, dectris_device_mapping(meta_filename))
         _compare_actual_and_expected_nexus_output(
-            hyperion_nexus,
-            example_nexus,
-            {
-                "entry": {
-                    "_missing": {"end_time"},
-                    "_extra": {"source", "end_time_estimated"},
-                    "data": {"_ignore": {"data", "omega"}},
-                    "instrument": {
-                        "_missing": {"transformations", "detector_z", "source"},
-                        "detector": {
-                            "_missing": {"detector_distance"},
-                            "threshold_energy": 1,
-                            "detector_readout_time": 1,
-                            "distance": 1,
-                            "transformations": 1,
-                            "detector_z": 1,
-                            "underload_value": 1,
-                            "bit_depth_image": 1,
-                            "_ignore": {
-                                "beam_center_x",
-                                "beam_center_y",
-                                "depends_on",
-                                "saturation_value",
-                                "sensor_material",
-                                "serial_number",
-                            },
-                            "detectorSpecific": {
-                                "_missing": {"pixel_mask"},
-                                "eiger_fw_version": 1,
-                                "x_pixels": 1,
-                                "ntrigger": 1,
-                                "data_collection_date": 1,
-                                "y_pixels": 1,
-                                "software_version": 1,
-                            },
-                            "module": {"_ignore": {"module_offset"}},
-                            "sensor_thickness": np.isclose,
-                        },
-                        "attenuator": {"_ignore": {"attenuator_transmission"}},
-                        "beam": {"_ignore": {"incident_wavelength"}},
-                        "name": b"DIAMOND BEAMLINE S03",
-                    },
-                    "sample": {
-                        "beam": {"incident_wavelength": np.isclose},
-                        "transformations": {
-                            "_missing": {"omega_end", "omega_increment_set"}
-                        },
-                        "sample_omega": {"omega_end": 1, "omega_increment_set": 1},
-                    },
-                    "end_time_estimated": b"test_timeZ",
-                    "start_time": b"test_timeZ",
-                    "source": 1,
-                }
-            },
+            hyperion_nexus, example_nexus, exceptions
         )
 
 
@@ -320,8 +369,13 @@ def _compare_actual_and_expected_nexus_output(actual, expected, exceptions: dict
 
 
 def _compare_actual_and_expected(path: list[str], actual, expected, exceptions: dict):
+    if expected is None:
+        # The nexus file under test contains a node that isn't in the original GDA reference nexus file
+        # but we may still expect something if the exception map contains it
+        expected = {}
+
     path_str = "/".join(path)
-    print(f"Comparing {path_str}")
+    LOGGER.debug(f"Comparing {path_str}")
     keys_not_in_actual = (
         expected.keys() - actual.keys() - exceptions.get("_missing", set())
     )
@@ -330,10 +384,12 @@ def _compare_actual_and_expected(path: list[str], actual, expected, exceptions: 
     ), f"Missing entries in group {path_str}, {keys_not_in_actual}"
 
     keys_to_compare = actual.keys()
+    keys_to_ignore = exceptions.get("_ignore", set())
     keys_not_in_expected = (
         keys_to_compare
         - expected.keys()
         - {k for k in exceptions.keys() if not k.startswith("_")}
+        - keys_to_ignore
     )
     cmp = len(keys_not_in_expected) == 0
     keys_to_compare = sorted(keys_to_compare)
@@ -342,33 +398,71 @@ def _compare_actual_and_expected(path: list[str], actual, expected, exceptions: 
         item_path = path + [key]
         actual_link = actual.get(key, getlink=True)
         item_path_str = "/" + "/".join(item_path)
+        exception = exceptions.get(key, None)
         if isinstance(actual_link, ExternalLink):
-            print(f"Skipping external link {item_path_str}")
+            if exception:
+                actual_link_path = f"{actual_link.filename}//{actual_link.path}"
+                assert (
+                    actual_link_path == exception
+                ), f"Actual and expected external links differ {actual_link_path}, {exception}"
+            else:
+                LOGGER.debug(
+                    f"Skipping external link {item_path_str} -> {actual_link.path}"
+                )
             continue
         actual_class = actual.get(key, getclass=True, getlink=False)
-        expected_class = expected.get(key, getclass=True)
+        expected_class = (
+            Group
+            if isinstance(exception, dict)
+            else (
+                Dataset if exception is not None else expected.get(key, getclass=True)  # type: ignore
+            )
+        )
         actual_value = actual.get(key)
-        expected_value = expected.get(key)
+        expected_value = (
+            expected.get(key)
+            if (exception is None or isinstance(exception, dict) or callable(exception))
+            else exception
+        )
         if expected_class == Group:
             _compare_actual_and_expected(
-                item_path, actual_value, expected_value, exceptions.get(key, {})
+                item_path, actual_value, expected.get(key), exceptions.get(key, {})
             )
-        elif expected_class == Dataset and key not in exceptions.get("_ignore", set()):
-            assert (
-                actual_value.shape == expected_value.shape
-            ), f"Actual and expected shapes differ for {item_path_str}: {actual_value.shape}, {expected_value.shape}"
+        elif (expected_class == Dataset) and key not in keys_to_ignore:
+            if isinstance(expected_value, Dataset):
+                # Only check shape if we didn't override the expected value
+                assert (
+                    actual_value.shape == expected_value.shape
+                ), f"Actual and expected shapes differ for {item_path_str}: {actual_value.shape}, {expected_value.shape}"
+            else:
+                assert hasattr(actual_value, "shape"), f"No shape for {item_path_str}"
+                expected_shape = np.shape(expected_value)  # type: ignore
+                assert (
+                    actual_value.shape == expected_shape
+                ), f"{item_path_str} data shape not expected shape{actual_value.shape}, {expected_shape}"
             if actual_value.shape == ():
-                exception = exceptions.get(key, None)
                 if callable(exception):
                     assert exceptions.get(key)(actual_value, expected_value)
                 elif np.isscalar(exception):
-                    assert actual_value[()] == exception
+                    assert (
+                        actual_value[()] == exception
+                    ), f"{item_path_str} actual and expected did not match {actual_value[()]}, {exception}."
                 else:
                     assert (
                         actual_class == expected_class
                     ), f"{item_path_str} Actual and expected class don't match {actual_class}, {expected_class}"
                     assert (
-                        actual_value[()] == expected_value[()]
-                    ), f"Actual and expected values differ for {item_path_str}: {actual_value[()]} != {expected_value[()]}"
+                        actual_value[()] == expected_value[()]  # type: ignore
+                    ), f"Actual and expected values differ for {item_path_str}: {actual_value[()]} != {expected_value[()]}"  # type: ignore
             else:
-                print(f"Ignoring non-scalar value {item_path_str}\n")
+                actual_value_str = np.array2string(actual_value, threshold=10)
+                expected_value_str = np.array2string(expected_value, threshold=10)  # type: ignore
+                if callable(exception):
+                    assert exception(
+                        actual_value, expected_value
+                    ), f"Actual and expected values differ for {item_path_str}: {actual_value_str} != {expected_value_str}"
+                else:
+                    assert np.array_equal(
+                        actual_value,
+                        expected_value,  # type: ignore
+                    ), f"Actual and expected values differ for {item_path_str}: {actual_value_str} != {expected_value_str}"
