dc62ad847a543f1fed379a7c48bd9002462409bb
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-05-15 10:52:33 +0100

    Fix external_callbacks system test

--- tests/system_tests/external_interaction/callbacks/test_external_callbacks.py
+++ tests/system_tests/external_interaction/callbacks/test_external_callbacks.py
@@ -98,8 +98,8 @@ def RE_with_external_callbacks():
         [
             "python",
             "src/hyperion/external_interaction/callbacks/__main__.py",
-            "--logging-level",
-            "DEBUG",
+            # "--logging-level",
+            # "DEBUG",
             "--dev",
         ],
         env=process_env,
