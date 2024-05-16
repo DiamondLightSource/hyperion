53a300251bf4ae95727b3ff4cb4a8002f5217f65
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-05-16 14:08:31 +0100

    Enhancements to build_imginfo.sh and run_imginfo.sh to enable building of the debug dockerfile and convenient launching

--- utility_scripts/build_imginfo.sh
+++ utility_scripts/build_imginfo.sh
@@ -5,6 +5,7 @@ function help {
       Create and build imginfo in the specified directory
 Options:
     -p use podman instead of docker
+    -d build debug imginfo
 `basename $0` --help|-h
       This help
 END
@@ -16,23 +17,40 @@ if [ -z "$1" ]; then
 fi
 
 DOCKER=docker
-case $1 in
-  --help|-h)
-    help
-    ;;
-  -p)
-    DOCKER=podman
-    shift
-    ;;
-  *)
+while [ -n "$1" ] ; do
+	case $1 in
+	  --help|-h)
+	    help
+	    ;;
+	  -d)
+	    DOCKERFILE=Dockerfile-imginfo-debug
+	    shift
+	    ;;
+	  -p)
+	    DOCKER=podman
+	    shift
+	    ;;
+	  *)
+	    break
+	    ;;
+	esac
+done
+if [ -z "$1" ]; then
+	echo "workspace not specified"
+	exit 1
+fi
 
-esac
 WORKDIR=$1
 IMGINFO_TAG=9810b92
 IMGINFO_GITHUB=git@github.com:githubgphl/imginfo.git
 if [ -d ${WORKDIR} ]; then
-        rm -rf ${WORKDIR}
+        rm -r ${WORKDIR}
 fi
 git clone ${IMGINFO_GITHUB} ${WORKDIR}
 git checkout ${IMGINFO_TAG}
-${DOCKER} build ${WORKDIR} --file ${WORKDIR}/Dockerfile --tag imginfo
+if [ -z "$DOCKERFILE" ]; then
+	DOCKERFILE=${WORKDIR}/Dockerfile
+else
+	DOCKERFILE=${WORKDIR}/${DOCKERFILE}
+fi
+${DOCKER} build ${WORKDIR} --file ${DOCKERFILE} --tag imginfo
