ad6649b4529278d807cb0ef0ecaecff274e0a5ac
David Perl <david.perl@diamond.ac.uk>
2024-05-24 09:22:30 +0100

    add test

--- tests/system_tests/external_interaction/test_config_service.py
+++ tests/system_tests/external_interaction/test_config_service.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import numpy as np
 import pytest
 from config_service.client import ConfigService
@@ -12,3 +14,14 @@ def test_get_beamline_params(config_service: ConfigService):
     resp = config_service.get_beamline_param("miniap_x_SMALL_APERTURE")
     assert isinstance(resp, float)
     assert np.isclose(resp, 2.459)
+
+
+def test_get_feature_flag(config_service: ConfigService):
+    resp = config_service.get_feature_flag("set_stub_offsets")
+    assert isinstance(resp, bool)
+    assert resp
+
+
+def test_nonsense_feature_flag_is_none(config_service: ConfigService):
+    resp = config_service.get_feature_flag(str(uuid4()))
+    assert resp is None
