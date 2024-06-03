338c71133e64831d6b7920f34e3cb14c58b826cf
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-06-03 11:35:17 +0100

    Tidy up move aperture tests

--- tests/unit_tests/device_setup_plans/test_manipulate_sample.py
+++ tests/unit_tests/device_setup_plans/test_manipulate_sample.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 from bluesky.run_engine import RunEngine
 from dodal.devices.aperturescatterguard import (
@@ -9,28 +9,25 @@ from dodal.devices.aperturescatterguard import (
 from hyperion.device_setup_plans.manipulate_sample import move_aperture_if_required
 
 
-@patch("bluesky.plan_stubs.abs_set")
 async def test_move_aperture_goes_to_correct_position(
-    mock_set: MagicMock, aperture_scatterguard: ApertureScatterguard, RE: RunEngine
+    aperture_scatterguard: ApertureScatterguard, RE: RunEngine
 ):
     assert aperture_scatterguard.aperture_positions
-
-    RE(
-        move_aperture_if_required(
-            aperture_scatterguard, AperturePositionGDANames.LARGE_APERTURE
+    with patch.object(aperture_scatterguard, "set") as mock_set:
+        RE(
+            move_aperture_if_required(
+                aperture_scatterguard, AperturePositionGDANames.LARGE_APERTURE
+            )
         )
-    )
-    mock_set.assert_called_once_with(
-        aperture_scatterguard,
-        aperture_scatterguard.aperture_positions.LARGE,
-        group="move_aperture",
-    )
+        mock_set.assert_called_once_with(
+            aperture_scatterguard.aperture_positions.LARGE,
+        )
 
 
 async def test_move_aperture_does_nothing_when_none_selected(
     aperture_scatterguard: ApertureScatterguard, RE: RunEngine
 ):
     assert aperture_scatterguard.aperture_positions
-    with patch("bluesky.plan_stubs.abs_set") as mock_set:
+    with patch.object(aperture_scatterguard, "set") as mock_set:
         RE(move_aperture_if_required(aperture_scatterguard, None))
         mock_set.assert_not_called()
