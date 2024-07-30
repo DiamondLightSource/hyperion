801a531128329df71bdc3127384450dd531d00ee
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-07-30 09:01:33 +0100

    (#842) Move the ispyb activation so that messages can be logged from pin tip detection

--- tests/unit_tests/external_interaction/callbacks/conftest.py
+++ tests/unit_tests/external_interaction/callbacks/conftest.py
@@ -8,6 +8,7 @@ from hyperion.parameters.gridscan import ThreeDGridScan
 from tests.conftest import create_dummy_scan_spec
 
 from ....conftest import default_raw_params, raw_params_from_file
+from ...conftest import OavGridSnapshotTestEvents
 
 
 def dummy_params():
@@ -32,7 +33,7 @@ def test_rotation_start_outer_document(dummy_rotation_params):
     }
 
 
-class TestData:
+class TestData(OavGridSnapshotTestEvents):
     DUMMY_TIME_STRING: str = "1970-01-01 00:00:00"
     GOOD_ISPYB_RUN_STATUS: str = "DataCollection Successful"
     BAD_ISPYB_RUN_STATUS: str = "DataCollection Unsuccessful"
@@ -129,11 +130,6 @@ class TestData:
         "zocalo_environment": "dev_artemis",
         "scan_points": create_dummy_scan_spec(10, 20, 30),
     }
-    test_descriptor_document_oav_snapshot: EventDescriptor = {
-        "uid": "b5ba4aec-de49-4970-81a4-b4a847391d34",
-        "run_start": "d8bee3ee-f614-4e7a-a516-25d6b9e87ef3",
-        "name": CONST.DESCRIPTORS.OAV_GRID_SNAPSHOT_TRIGGERED,
-    }  # type: ignore
     test_descriptor_document_oav_rotation_snapshot: EventDescriptor = {
         "uid": "c7d698ce-6d49-4c56-967e-7d081f964573",
         "run_start": "d8bee3ee-f614-4e7a-a516-25d6b9e87ef3",
@@ -161,46 +157,6 @@ class TestData:
         "seq_num": 1,
         "uid": "32d7c25c-c310-4292-ac78-36ce6509be3d",
         "data": {"oav_snapshot_last_saved_path": "snapshot_0"},
-    }
-    test_event_document_oav_snapshot_xy: Event = {
-        "descriptor": "b5ba4aec-de49-4970-81a4-b4a847391d34",
-        "time": 1666604299.828203,
-        "timestamps": {},
-        "seq_num": 1,
-        "uid": "29033ecf-e052-43dd-98af-c7cdd62e8174",
-        "data": {
-            "oav_grid_snapshot_top_left_x": 50,
-            "oav_grid_snapshot_top_left_y": 100,
-            "oav_grid_snapshot_num_boxes_x": 40,
-            "oav_grid_snapshot_num_boxes_y": 20,
-            "oav_grid_snapshot_microns_per_pixel_x": 1.25,
-            "oav_grid_snapshot_microns_per_pixel_y": 1.5,
-            "oav_grid_snapshot_box_width": 0.1 * 1000 / 1.25,  # size in pixels
-            "oav_grid_snapshot_last_path_full_overlay": "test_1_y",
-            "oav_grid_snapshot_last_path_outer": "test_2_y",
-            "oav_grid_snapshot_last_saved_path": "test_3_y",
-            "smargon-omega": 0,
-        },
-    }
-    test_event_document_oav_snapshot_xz: Event = {
-        "descriptor": "b5ba4aec-de49-4970-81a4-b4a847391d34",
-        "time": 1666604299.828203,
-        "timestamps": {},
-        "seq_num": 1,
-        "uid": "29033ecf-e052-43dd-98af-c7cdd62e8174",
-        "data": {
-            "oav_grid_snapshot_top_left_x": 50,
-            "oav_grid_snapshot_top_left_y": 0,
-            "oav_grid_snapshot_num_boxes_x": 40,
-            "oav_grid_snapshot_num_boxes_y": 10,
-            "oav_grid_snapshot_box_width": 0.1 * 1000 / 1.25,  # size in pixels
-            "oav_grid_snapshot_last_path_full_overlay": "test_1_z",
-            "oav_grid_snapshot_last_path_outer": "test_2_z",
-            "oav_grid_snapshot_last_saved_path": "test_3_z",
-            "oav_grid_snapshot_microns_per_pixel_x": 1.25,
-            "oav_grid_snapshot_microns_per_pixel_y": 1.5,
-            "smargon-omega": -90,
-        },
     }
     test_event_document_pre_data_collection: Event = {
         "descriptor": "bd45c2e5-2b85-4280-95d7-a9a15800a78b",
