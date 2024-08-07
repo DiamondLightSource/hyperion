67c942167a6e3c5d7fc3397bd29e88b36c9c07f5
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-08-07 14:28:26 +0100

    Ensure the appVersion is set in the helmchart. Update documentation. Allow production to mount source folders.

--- utility_scripts/deploy/deploy_to_k8s.sh
+++ utility_scripts/deploy/deploy_to_k8s.sh
@@ -62,6 +62,21 @@ hyperion.logDir=/project/tmp,\
 hyperion.projectDir=$PROJECTDIR \
 --replace "
   mkdir -p $PROJECTDIR/tmp
+elif [[ ! -e $PROJECTDIR/src/hyperion/_version.py ]]; then
+  # Production install requires the _version.py to be created, this needs a minimal virtual environment
+  echo "Creating _version.py"
+  if [[ -d $PROJECTDIR/.venv/bin/activate ]]; then
+    echo "Virtual environment not found - creating"
+    module load python/3.11
+    python -m venv $PROJECTDIR/.venv
+    . $PROJECTDIR/.venv/bin/activate
+    pip install setuptools_scm
+  else
+    . $PROJECTDIR/.venv/bin/activate
+  fi
+  python -m setuptools_scm --force-write-version-files
 fi
-
-helm install $HELM_OPTIONS $RELEASE $PROJECTDIR/helmchart
+APP_VERSION=`python -m setuptools_scm | sed -e 's/[^a-zA-Z0-9._-]/_/g'`
+HELM_OPTIONS+="--set hyperion.appVersion=$APP_VERSION "
+helm package $PROJECTDIR/helmchart --app-version $APP_VERSION
+helm install $HELM_OPTIONS $RELEASE hyperion-0.0.1.tgz
